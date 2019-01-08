9b97f7e7e92b23ee70c39d8936b1f30797afa09a
wangshijun <wangshijun2010@gmail.com>
2019-01-08 15:59:49 +0800

    feat: support both stream and unary grpc calls

--- packages/forge-node/test.js
+++ packages/forge-node/test.js
@@ -1,23 +1,69 @@
-var messages = require('@arcblock/forge-proto/lib/rpc_pb');
-var services = require('@arcblock/forge-proto/lib/rpc_grpc_pb');
+/* eslint no-console:"off" */
+const grpc = require('grpc');
+const rpcTypes = require('@arcblock/forge-proto/lib/rpc_pb');
+const rpcClients = require('@arcblock/forge-proto/lib/rpc_grpc_pb');
+const txTypes = require('@arcblock/forge-proto/lib/tx_pb');
+const eventTypes = require('@arcblock/forge-proto/lib/event_pb');
+const eventClients = require('@arcblock/forge-proto/lib/event_grpc_pb');
+const enumTypes = require('@arcblock/forge-proto/lib/type_pb');
+const codeTypes = require('@arcblock/forge-proto/lib/code_pb');
 
-var grpc = require('grpc');
+const constants = Object.freeze({
+  StatusCode: codeTypes.StatusCode,
+  KeyType: enumTypes.KeyType,
+  HashType: enumTypes.HashType,
+  EncodingType: enumTypes.EncodingType,
+  UpgradeAction: enumTypes.UpgradeAction,
+  UpgradeType: enumTypes.UpgradeType,
+});
 
-console.log({ services: Object.keys(services) });
-console.log({ StateRpcService: services.StateRpcService });
+console.log({ clients: Object.keys(rpcClients).concat(Object.keys(eventClients)), constants });
 
-function main() {
-  var client = new services.StateRpcClient('127.0.0.1:28210', grpc.credentials.createInsecure());
-  var request = new messages.RequestGetForgeState();
-  request.setKey('height');
-  request.setAppHash('56E81F171BCC55A6FF8345E692C0F86E5B48E01B996CADC001622FB5E363B421');
-  console.log({ client, request: request.toObject() });
-  client.get_forge_state(request, function(err, response) {
+// Unary
+function getChainInfo() {
+  const client = new rpcClients.ChainRpcClient(
+    '127.0.0.1:28210',
+    grpc.credentials.createInsecure()
+  );
+  const request = new rpcTypes.RequestGetChainInfo();
+  // console.log({ client, request: request.toObject() });
+  client.get_chain_info(request, function(err, response) {
     if (err) {
       return console.error(err);
     }
-    console.log('state:', { response: response.toObject() });
+    console.log('chainInfo:', { response: response.toObject() });
   });
 }
 
-main();
+// Streaming
+function getBlockByHeight() {
+  const client = new rpcClients.ChainRpcClient(
+    '127.0.0.1:28210',
+    grpc.credentials.createInsecure()
+  );
+
+  const stream = client.get_block();
+
+  stream
+    .on('data', function(response) {
+      const { code, block } = response.toObject();
+      if (code) {
+        throw new Error('response error', code);
+      }
+      console.log('blockInfo:', block);
+    })
+    .on('status', status => {
+      console.info('status', status);
+    })
+    .on('error', err => {
+      console.error('error', err);
+    });
+
+  const request = new rpcTypes.RequestGetBlock();
+  request.setHeight(2);
+  stream.write(request);
+  stream.end();
+}
+
+getChainInfo();
+getBlockByHeight();
