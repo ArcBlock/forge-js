9815f448f8e42582b0a8ad6a78a412c5637052aa
wangshijun <wangshijun2010@gmail.com>
2019-01-08 18:38:39 +0800

    feat: basic api generation process

--- packages/forge-node/test.js
+++ packages/forge-node/test.js
@@ -1,69 +1,189 @@
 /* eslint no-console:"off" */
 const grpc = require('grpc');
+const camelcase = require('camelcase');
+const { EventEmitter } = require('events');
+const types = require('@arcblock/forge-proto/lib/type_pb');
 const rpcTypes = require('@arcblock/forge-proto/lib/rpc_pb');
 const rpcClients = require('@arcblock/forge-proto/lib/rpc_grpc_pb');
-const txTypes = require('@arcblock/forge-proto/lib/tx_pb');
-const eventTypes = require('@arcblock/forge-proto/lib/event_pb');
+// const txTypes = require('@arcblock/forge-proto/lib/tx_pb');
+// const eventTypes = require('@arcblock/forge-proto/lib/event_pb');
 const eventClients = require('@arcblock/forge-proto/lib/event_grpc_pb');
-const enumTypes = require('@arcblock/forge-proto/lib/type_pb');
 const codeTypes = require('@arcblock/forge-proto/lib/code_pb');
+// const abi = require('@arcblock/forge-proto/lib/rpc.pb').forge_abi;
+const debug = require('debug')(require('./package.json').name);
 
 const constants = Object.freeze({
   StatusCode: codeTypes.StatusCode,
-  KeyType: enumTypes.KeyType,
-  HashType: enumTypes.HashType,
-  EncodingType: enumTypes.EncodingType,
-  UpgradeAction: enumTypes.UpgradeAction,
-  UpgradeType: enumTypes.UpgradeType,
+  KeyType: types.KeyType,
+  HashType: types.HashType,
+  EncodingType: types.EncodingType,
+  UpgradeAction: types.UpgradeAction,
+  UpgradeType: types.UpgradeType,
 });
 
-console.log({ clients: Object.keys(rpcClients).concat(Object.keys(eventClients)), constants });
+// const encoders = Object.keys(abi).filter(x => x.includes('Request')).reduce((memo, x) => {
+//   memo[x] = abi[x];
+//   return memo;
+// }, {});
 
-// Unary
-function getChainInfo() {
-  const client = new rpcClients.ChainRpcClient(
-    '127.0.0.1:28210',
-    grpc.credentials.createInsecure()
-  );
-  const request = new rpcTypes.RequestGetChainInfo();
-  // console.log({ client, request: request.toObject() });
-  client.get_chain_info(request, function(err, response) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log('chainInfo:', { response: response.toObject() });
-  });
-}
+// console.log(encoders);
 
-// Streaming
-function getBlockByHeight() {
-  const client = new rpcClients.ChainRpcClient(
-    '127.0.0.1:28210',
-    grpc.credentials.createInsecure()
-  );
+const specs = Object.assign({}, rpcClients, eventClients);
+const clients = Object.keys(specs)
+  .filter(x => x.includes('Client'))
+  .reduce((memo, x) => {
+    memo[x] = new specs[x]('127.0.0.1:28210', grpc.credentials.createInsecure());
 
-  const stream = client.get_block();
+    return memo;
+  }, {});
 
-  stream
-    .on('data', function(response) {
-      const { code, block } = response.toObject();
-      if (code) {
-        throw new Error('response error', code);
+const sdk = Object.keys(specs)
+  .filter(x => x.includes('Service'))
+  .reduce((memo, x) => {
+    Object.keys(specs[x]).forEach(m => {
+      const client = clients[x.replace('Service', 'Client')];
+      const method = camelcase(m);
+      const spec = specs[x][m];
+      const { requestStream, responseStream, requestType } = spec;
+
+      const createRequest = (RequestType, params = {}) => {
+        const request = new RequestType();
+        if (params && typeof params === 'object') {
+          // FIXME: support complex/nested types
+          Object.keys(params).forEach(key => {
+            const fn = camelcase(`set_${key}`);
+            request[fn](params[key]);
+          });
+        }
+
+        debug('createRequest', { method, request: request.toObject() });
+        return request;
+      };
+
+      const createResponseHandler = (resolve, reject) => (err, response) => {
+        if (err) {
+          return reject(err);
+        }
+
+        const res = response.toObject();
+        if (res.code) {
+          return reject(new Error(`gRPC response error ${m} => ${method}, errorCode: ${res.code}`));
+        }
+
+        return resolve(res);
+      };
+
+      const wrapStreamResponse = stream => {
+        const emitter = new EventEmitter();
+
+        stream
+          .on('data', function(response) {
+            const res = response.toObject();
+            if (res.code) {
+              emitter.emit('error', new Error(`gRPC response error: ${res.code}`));
+              return;
+            }
+            emitter.emit('data', res);
+          })
+          .on('error', err => {
+            emitter.emit('error', err);
+          });
+
+        return emitter;
+      };
+
+      debug('generate', { method, requestStream, responseStream });
+
+      // unary call, return Promise
+      if (requestStream === false && responseStream === false) {
+        memo[method] = params =>
+          new Promise((resolve, reject) => {
+            const request = createRequest(requestType, params);
+            client[m](request, createResponseHandler(resolve, reject));
+          });
+
+        // response streaming: return EventEmitter, TODO: wrap stream handling?
+      } else if (requestStream === false && responseStream) {
+        memo[method] = params => {
+          const request = createRequest(requestType, params);
+          const stream = client[m](request);
+          const emitter = wrapStreamResponse(stream);
+          return emitter;
+        };
+
+        // request streaming: return Promise
+      } else if (requestStream && responseStream === false) {
+        memo[method] = params =>
+          new Promise((resolve, reject) => {
+            const request = createRequest(requestType, params);
+            const stream = client[m](createResponseHandler(resolve, reject));
+            stream.write(request);
+            stream.end();
+          });
+
+        // request & response streaming: return EventEmitter
+      } else {
+        memo[method] = params => {
+          const request = createRequest(requestType, params);
+          const stream = client[m]();
+          const emitter = wrapStreamResponse(stream);
+          stream.write(request);
+          stream.end();
+          return emitter;
+        };
       }
-      console.log('blockInfo:', block);
-    })
-    .on('status', status => {
-      console.info('status', status);
-    })
-    .on('error', err => {
-      console.error('error', err);
+
+      memo[method].spec = spec;
     });
+    return memo;
+  }, {});
 
-  const request = new rpcTypes.RequestGetBlock();
-  request.setHeight(2);
-  stream.write(request);
-  stream.end();
-}
+(async () => {
+  try {
+    const res = await sdk.getChainInfo();
+    debug('chainInfo', res.info);
 
-getChainInfo();
-getBlockByHeight();
+    const stream = sdk.getBlock({ height: 11 });
+    stream
+      .on('data', function({ block }) {
+        debug('blockInfo:', block);
+      })
+      .on('error', err => {
+        console.error('error', err);
+      });
+  } catch (err) {
+    console.error('error', err);
+  }
+})();
+
+// Send transaction
+// function createWallet() {
+//   const walletType = new types.WalletType();
+//   walletType.setPk(constants.KeyType.SECP256K1);
+//   walletType.setHash(constants.HashType.KECCAK);
+//   walletType.setAddress(constants.EncodingType.BASE16);
+
+//   const request = new rpcTypes.RequestCreateWallet();
+//   request.setPassphrase('123456');
+//   request.setType(walletType);
+//   request.setMoniker('wangshijun');
+
+//   const client = new rpcClients.WalletRpcClient(
+//     '127.0.0.1:28210',
+//     grpc.credentials.createInsecure()
+//   );
+
+//   client.create_wallet(request, (err, response) => {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     const { code, token, wallet } = response.toObject();
+//     if (code) {
+//       throw new Error('wallet create error', code);
+//     }
+//     console.log('sessionToken:', token);
+//     console.log('walletInfo:', wallet);
+//   });
+// }
+
+// createWallet();
