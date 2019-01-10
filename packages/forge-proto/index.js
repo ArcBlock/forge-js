e68bd47abc8ffb134774e2a1f8374dba6061e67a
wangshijun <wangshijun2010@gmail.com>
2019-01-10 09:53:48 +0800

    chore: refactor forge-proto, expose utility methods

--- packages/forge-proto/index.js
+++ packages/forge-proto/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const json = require('./lib/spec.json');
 
 // extract spec
 const compactSpec = object => {
@@ -16,69 +15,95 @@ const compactSpec = object => {
 
   return object;
 };
-const spec = compactSpec(json);
 
 // extract types and services
-const types = {};
-const vendorTypes = {};
-const services = {};
-fs.readdirSync(path.join(__dirname, './lib/'))
-  .filter(x => /\.js$/.test(x))
-  .forEach(x => {
-    if (/^vendor_/.test(x)) {
-      if (/_grpc_pb\.js$/.test(x)) {
-        // Ignore vendor services
-      } else if (/_pb\.js$/.test(x)) {
-        const content = require(path.join(__dirname, './lib/', x));
-        Object.assign(vendorTypes, content);
+const processJs = baseDir => {
+  const types = {};
+  const vendorTypes = {};
+  const services = {};
+  fs.readdirSync(baseDir)
+    .filter(x => /\.js$/.test(x))
+    .forEach(x => {
+      const filePath = path.join(baseDir, x);
+      if (/^vendor_/.test(x)) {
+        if (/_grpc_pb\.js$/.test(x)) {
+          // Ignore vendor services
+        } else if (/_pb\.js$/.test(x)) {
+          const content = require(filePath);
+          Object.assign(vendorTypes, content);
+        }
+      } else {
+        const content = require(filePath);
+        if (/_grpc_pb\.js$/.test(x)) {
+          Object.assign(services, content);
+        } else if (/_pb\.js$/.test(x)) {
+          Object.assign(types, content);
+        }
       }
-    } else {
-      const content = require(path.join(__dirname, './lib/', x));
-      if (/_grpc_pb\.js$/.test(x)) {
-        Object.assign(services, content);
-      } else if (/_pb\.js$/.test(x)) {
-        Object.assign(types, content);
-      }
-    }
-  });
+    });
 
-// extract client constructors
-const clients = Object.keys(services)
-  .filter(x => x.includes('Client'))
-  .map(x => x.replace(/Client$/, ''))
-  .reduce((clients, x) => {
-    clients[x] = services[`${x}Client`];
-    return clients;
-  }, {});
-
-// extract messages and enums
-const { forge_abi: abi } = spec;
-const messages = {};
-const enums = Object.keys(abi)
-  .filter(x => abi[x].values)
-  .reduce((enums, x) => {
-    messages[x] = {};
-    enums[x] = Object.keys(abi[x].values).reduce((values, k) => {
-      values[k.toUpperCase()] = abi[x].values[k];
-      messages[x][abi[x].values[k]] = k.toUpperCase();
-      return values;
+  // extract client constructors
+  const clients = Object.keys(services)
+    .filter(x => x.includes('Client'))
+    .map(x => x.replace(/Client$/, ''))
+    .reduce((clients, x) => {
+      clients[x] = services[`${x}Client`];
+      return clients;
     }, {});
 
-    return enums;
-  }, {});
+  return { types, vendorTypes, services, clients };
+};
 
-// extract rpcs
-const rpcs = Object.keys(abi)
-  .filter(x => abi[x].methods)
-  .reduce((rpcs, x) => {
-    rpcs[x] = abi[x].methods;
-    return rpcs;
-  }, {});
+// extract rpc descriptors
+const processJson = filePath => {
+  const json = fs.existsSync(filePath) ? require(filePath) : json;
+  const spec = compactSpec(json);
+  Object.assign(spec, spec.forge_abi);
 
-Object.assign(spec, spec.forge_abi);
-delete spec.forge_abi;
+  const { forge_abi: abi } = spec;
 
-module.exports = Object.freeze({ enums, messages, rpcs, clients, types, vendorTypes, spec });
+  // extract messages and enums
+  const messages = {};
+  const enums = Object.keys(abi)
+    .filter(x => abi[x].values)
+    .reduce((enums, x) => {
+      messages[x] = {};
+      enums[x] = Object.keys(abi[x].values).reduce((values, k) => {
+        values[k.toUpperCase()] = abi[x].values[k];
+        messages[x][abi[x].values[k]] = k.toUpperCase();
+        return values;
+      }, {});
+
+      return enums;
+    }, {});
+
+  // extract rpcs
+  const rpcs = Object.keys(abi)
+    .filter(x => abi[x].methods)
+    .reduce((rpcs, x) => {
+      rpcs[x] = abi[x].methods;
+      return rpcs;
+    }, {});
+
+  return { messages, enums, rpcs, spec };
+};
+
+const { types, vendorTypes, clients } = processJs(path.resolve(__dirname, './lib/'));
+const { messages, enums, rpcs, spec } = processJson(path.resolve(__dirname, './lib/spec.json'));
+
+module.exports = Object.freeze({
+  enums,
+  messages,
+  rpcs,
+  clients,
+  types,
+  vendorTypes,
+  spec,
+  compactSpec,
+  processJs,
+  processJson,
+});
+
 // console.log({ rpc: rpcs.ChainRpc, client: clients.ChainRpc, service: services.ChainRpcService });
 // console.log({ rpcs, clients, service: services.ChainRpcService });
 // console.log(spec);
