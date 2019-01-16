75b49ae802b7c3c045c8b3bf69c66896c1668544
wangshijun <wangshijun2010@gmail.com>
2019-01-16 11:54:17 +0800

    chore: add error handling for chain-info request in forge-cli

--- packages/forge-cli/src/cli/chain/info/info.js
+++ packages/forge-cli/src/cli/chain/info/info.js
@@ -1,9 +1,15 @@
 const shell = require('shelljs');
 const pretty = require('json-stringify-pretty-compact');
 const { client } = require('core/env');
+const { symbols } = require('core/ui');
 
-function getChainInfo() {
-  client.getChainInfo().then(res => shell.echo(pretty(res.$format().info)));
+async function getChainInfo() {
+  try {
+    const res = await client.getChainInfo();
+    shell.echo(pretty(res.$format().info));
+  } catch (err) {
+    shell.echo(`${symbols.error} rpc request field: ${err.message}`);
+  }
 }
 
 exports.run = getChainInfo;
