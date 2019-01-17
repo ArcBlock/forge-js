d9e07eb45ba6ef6d40a476a768130ebb328e091d
NateRobinson <840501291@qq.com>
2019-01-17 15:55:09 +0800

    add account delete cli

---
+++ packages/forge-cli/src/cli/account/delete/delete.js
@@ -0,0 +1,27 @@
+const shell = require('shelljs');
+const { client } = require('core/env');
+const { symbols } = require('core/ui');
+const pretty = require('json-stringify-pretty-compact');
+
+async function execute({ args: [address] }) {
+  if (!address) {
+    shell.echo(`${symbols.error} Please input an account address and try again.`);
+    return;
+  }
+  try {
+    const stream = await client.removeWallet({ address: address });
+    stream
+      .on('data', function(result) {
+        shell.echo(`${symbols.success} remove account success: `);
+        shell.echo(`${pretty(result)}`);
+      })
+      .on('error', err => {
+        shell.echo(`${symbols.error} error: ${err}`);
+      });
+  } catch (err) {
+    shell.echo(`${symbols.error} error: ${err}`);
+  }
+}
+
+exports.run = execute;
+exports.execute = execute;
