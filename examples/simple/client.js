c2be5e04625258bf135bee161e99dc982ba2c863
wangshijun <wangshijun2010@gmail.com>
2019-01-18 16:29:59 +0800

    [simple-app] chore: make rpc client work with latest forge

--- examples/simple/client.js
+++ examples/simple/client.js
@@ -3,14 +3,27 @@ const path = require('path');
 const { enums } = require('@arcblock/forge-proto');
 const { RpcClient, parseConfig } = require('@arcblock/forge-sdk');
 const client = new RpcClient(parseConfig(path.resolve(__dirname, './forge.toml')));
+
 const debug = (...args) => {
-  console.log('x'.repeat(80));
-  console.log(...args);
   console.log('');
+  console.log('-'.repeat(80));
+  console.log('');
+  console.log(...args);
 };
 
-debug('Supported RPC methods', client.listRpcMethods());
+// Because creating an wallet is asynchronous we need to add some delay to retrieve user info
+const delay = (timeout = 1000) => new Promise(resolve => setTimeout(resolve, timeout));
+const random = prefix => `${prefix}_${Math.round(Math.random() * 1000000)}`;
+
+debug('Supported RPC methods', Object.keys(client.listRpcMethods()));
 debug('Supported Tx methods', client.listTxMethods());
+
+const getAccountState = address => {
+  const account = client.getAccountState([{ address }]);
+  account.on('data', data => {
+    debug(`accountInfo.${address}`, data.$format().state);
+  });
+};
 
 (async () => {
   try {
@@ -18,14 +31,10 @@ debug('Supported Tx methods', client.listTxMethods());
     const res = await client.getChainInfo();
     debug('chainInfo', res.$format());
 
-    const stream = client.getBlock({ height: 1 });
-    stream
-      .on('data', function({ block }) {
-        debug('blockInfo:', block);
-      })
-      .on('error', err => {
-        debug('error', err);
-      });
+    const stream = client.getBlock({ height: res.info.blockHeight - 1 });
+    stream.on('data', function({ block }) {
+      debug('blockInfo:', block);
+    });
 
     // WalletRpc
     const walletType = {
@@ -35,34 +44,19 @@ debug('Supported Tx methods', client.listTxMethods());
     };
     const sender = await client.createWallet({
       passphrase: '123456',
-      moniker: 'wangshijun',
+      moniker: random('wangshijun'),
       type: walletType,
     });
+    await delay();
+    getAccountState(sender.wallet.address);
+
     const receiver = await client.createWallet({
       passphrase: '123456',
-      moniker: 'tyrchen',
+      moniker: random('tyrchen'),
       type: walletType,
     });
-
-    // Send transaction the hard way
-    const { tx } = await client.createTx({
-      from: sender.wallet.address,
-      token: sender.token,
-      nonce: 2,
-      itx: {
-        type: 'TransferTx',
-        value: {
-          to: receiver.wallet.address,
-          value: client.toArc(10),
-        },
-      },
-    });
-    const { hash: transferHash } = await client.sendTx({
-      tx,
-      token: sender.token,
-      commit: true,
-    });
-    debug('txInfo.sent.multiStep', { transferHash });
+    await delay();
+    getAccountState(receiver.wallet.address);
 
     // Send transaction the easy way
     const hash = await client.sendTransferTx({
@@ -75,12 +69,8 @@ debug('Supported Tx methods', client.listTxMethods());
     });
     debug('txInfo.sent.oneShot', { hash });
 
-    const account = client.getAccountState({
-      address: sender.wallet.address,
-    });
-    account.on('data', data => {
-      debug('accountInfo', data.$format());
-    });
+    await delay();
+    getAccountState(sender.wallet.address);
   } catch (err) {
     console.error('error', err);
   }
