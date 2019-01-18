b892fc4c567e6bc7b64559b9fb10a65096015ea1
wangshijun <wangshijun2010@gmail.com>
2019-01-18 11:38:33 +0800

    chore: add forge config command

--- packages/forge-cli/src/cli/node/index.js
+++ packages/forge-cli/src/cli/node/index.js
@@ -1,8 +1,9 @@
+exports.config = require('./config');
 exports.console = require('./console');
 exports.debug = require('./debug');
+exports.init = require('./init');
 exports.ps = require('./ps');
 exports.reboot = require('./reboot');
 exports.restart = require('./restart');
 exports.start = require('./start');
 exports.stop = require('./stop');
-exports.init = require('./init');
