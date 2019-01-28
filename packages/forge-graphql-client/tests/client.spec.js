b516e7e6c545d0f75bcd9dea42561cbff5a7f783
wangshijun <wangshijun2010@gmail.com>
2019-01-28 17:16:27 +0800

    chore: rename folders

--- packages/forge-graphql-client/tests/client.spec.js
+++ packages/forge-graphql-client/tests/client.spec.js
@@ -1,6 +1,6 @@
 const GraphqlClient = require('../src/client');
 
-describe('forge-graphql-client', () => {
+describe('GraphqlClient', () => {
   test('should be a function', () => {
     expect(typeof GraphqlClient).toEqual('function');
   });
