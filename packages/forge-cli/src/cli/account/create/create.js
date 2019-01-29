43fc06a6563a2716bb5b5c5dcac0a30f85ce6199
wangshijun <wangshijun2010@gmail.com>
2019-01-30 07:13:14 +0800

    fix: format validation error #103

--- packages/forge-cli/src/cli/account/create/create.js
+++ packages/forge-cli/src/cli/account/create/create.js
@@ -29,7 +29,7 @@ const questions = [
     message: 'Please input passphrase:',
     validate: input => {
       if (!input) return 'The passphrase should not empty';
-      if (!/^.{6,15}$/.test(input)) return 'The passphrase format validation error';
+      if (!/^.{6,15}$/.test(input)) return 'The passphrase length should between 6~25 char';
       return true;
     },
   },
@@ -40,7 +40,7 @@ const questions = [
     validate: input => {
       if (!input) return 'The moniker should not empty';
       if (!/^[a-zA-Z0-9][a-zA-Z0-9_]{3,15}$/.test(input))
-        return 'The moniker format validation error';
+        return 'The moniker should only contain 0-9,a-z,A-Z, and length between 3~15';
       return true;
     },
   },
