6ec59038f1585415629ae223b8a953084cbdbf00
wangshijun <wangshijun2010@gmail.com>
2019-01-30 08:59:14 +0800

    chore: basic block list loading

--- apps/forge-web/src/pages/node/explorer.js
+++ apps/forge-web/src/pages/node/explorer.js
@@ -2,21 +2,69 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Page from '../../components/page';
 import Layout from '../../layouts/page';
 import withI18n from '../../components/withI18n';
 import withRoot from '../../components/withRoot';
+import forge from '../../libs/forge';
 
-class Explorer extends Page {
+class BlockList extends Page {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      blocks: null,
+      chainInfo: null,
+    };
+  }
+
+  componentDidMount() {
+    this.loadStatus();
+  }
+
   render() {
+    const { loading, chainInfo, blocks } = this.state;
     return (
-      <Layout title="Explorer" cookies={this.cookies}>
+      <Layout title="BlockList" cookies={this.cookies}>
         <Container>
-          <Typography component="h3">Node Explorer here...</Typography>
+          <Typography component="h3">BlockList here...</Typography>
+          {loading && <CircularProgress />}
+          {chainInfo && (
+            <div>
+              <Typography component="h3">Chain Info</Typography>
+              <pre>
+                <code>{JSON.stringify(chainInfo, true, '  ')}</code>
+              </pre>
+            </div>
+          )}
+          {blocks && (
+            <div>
+              <Typography component="h3">Block List</Typography>
+              <pre>
+                <code>{JSON.stringify(blocks, true, '  ')}</code>
+              </pre>
+            </div>
+          )}
         </Container>
       </Layout>
     );
+  }
+
+  async loadStatus() {
+    this.setState({ loading: true });
+    const {
+      getChainInfo: { info: chainInfo },
+    } = await forge.getChainInfo();
+    console.log(chainInfo);
+    const { blockHeight } = chainInfo;
+    const {
+      getBlock: { block },
+    } = await forge.getBlock({ height: blockHeight });
+    console.log(block);
+
+    this.setState({ loading: false, chainInfo, blocks: [block] });
   }
 }
 
@@ -24,4 +72,4 @@ const Container = styled.div`
   padding: ${props => props.theme.spacing.unit * 3}px;
 `;
 
-export default withRoot(withI18n(Explorer));
+export default withRoot(withI18n(BlockList));
