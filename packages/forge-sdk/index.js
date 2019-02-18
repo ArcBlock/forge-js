902c145b5cc5900a03dc8f20c2001ab2d9a6b67b
wangshijun <wangshijun2010@gmail.com>
2019-02-18 10:17:56 +0800

    fix: make j2d work for forge-sdk entry file

--- packages/forge-sdk/index.js
+++ packages/forge-sdk/index.js
@@ -19,7 +19,7 @@ const {
   decodePayload,
 } = require('./lib/util/socket_data');
 
-module.exports = Object.assign({
+module.exports = {
   RpcClient: require('./lib/client/rpc'),
   TcpServer: require('./lib/server/tcp'),
 
@@ -45,4 +45,4 @@ module.exports = Object.assign({
   encodeSocketData,
   decodeSocketData,
   decodePayload,
-});
+};
