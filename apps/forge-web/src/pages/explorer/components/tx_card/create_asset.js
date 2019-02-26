7abd7352aef1f25e5a198cd962e08efe7e8e0c90
wangshijun <wangshijun2010@gmail.com>
2019-02-26 12:02:45 +0800

    chore: improve update asset

--- apps/forge-web/src/pages/explorer/components/tx_card/create_asset.js
+++ apps/forge-web/src/pages/explorer/components/tx_card/create_asset.js
@@ -10,24 +10,39 @@ import Typography from '@material-ui/core/Typography';
 
 import IconFa from '../../../../components/iconfa';
 
-const CreateAssetTx = React.memo(({ tx, type, theme, ...rest }) => (
-  <Container {...rest}>
-    <div className="info-row">
-      <Typography component="p" className="hash" title={tx.hash}>
-        <Link to={`/node/explorer/txs/${tx.hash}`}># {tx.hash}</Link>
-      </Typography>
-      <Typography component="p" className="time" title={tx.time}>
-        {moment(tx.time).fromNow()}
-      </Typography>
-    </div>
-    <div className="info-row" style={{ justifyContent: 'flex-start' }}>
-      <IconFa name="gem" size={14} rounded={true} color={theme.colors.blue} className="type-icon" />
-      <Typography component="p" className="value" title={get(tx, 'tx.itx.data.value', 'NO DATA')}>
-        {type}: {get(tx, 'tx.itx.data.value', 'NO DATA')}
-      </Typography>
-    </div>
-  </Container>
-));
+const CreateAssetTx = React.memo(({ tx, type, theme, ...rest }) => {
+  const address = get(tx, 'tx.itx.address', '');
+  const data = get(tx, 'tx.itx.data.value', 'NO DATA');
+  return (
+    <Container {...rest}>
+      <div className="info-row">
+        <Typography component="p" className="hash" title={tx.hash}>
+          <Link to={`/node/explorer/txs/${tx.hash}`}># {tx.hash}</Link>
+        </Typography>
+        <Typography component="p" className="time" title={tx.time}>
+          {moment(tx.time).fromNow()}
+        </Typography>
+      </div>
+      <div className="info-row" style={{ justifyContent: 'flex-start' }}>
+        <IconFa
+          name="gem"
+          size={14}
+          rounded={true}
+          color={theme.colors.blue}
+          className="type-icon"
+        />
+        <Typography component="p" className="value" title={data}>
+          {type}: {data}
+        </Typography>
+        {address && (
+          <Typography component="p" className="address" title={address}>
+            <Link to={`/node/explorer/assets/${address}`}>{address}</Link>
+          </Typography>
+        )}
+      </div>
+    </Container>
+  );
+});
 
 const Container = styled.div`
   .info-row {
