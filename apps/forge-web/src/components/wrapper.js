36735748ac214dbc0345ffd9fc2b7adecb9960bc
wangshijun <wangshijun2010@gmail.com>
2019-03-01 15:41:19 +0800

    chore: make content centered

--- apps/forge-web/src/components/wrapper.js
+++ apps/forge-web/src/components/wrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export default styled.div`
   padding: ${props => props.theme.spacing.unit * 6}px 8%;
   width: auto;
-  max-width: 1280px;
+  max-width: 1440px;
   box-sizing: border-box;
   margin: 0 auto;
 `;
