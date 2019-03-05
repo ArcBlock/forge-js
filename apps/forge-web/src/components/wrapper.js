7caed410347e7af6b9ad32fd024ba17b6335f783
wangshijun <wangshijun2010@gmail.com>
2019-03-05 10:10:29 +0800

    chore: fix page width

--- apps/forge-web/src/components/wrapper.js
+++ apps/forge-web/src/components/wrapper.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  padding: ${props => props.theme.spacing.unit * 6}px 8%;
-  width: auto;
-  max-width: 1440px;
+  padding: ${props => props.theme.spacing.unit * 6}px 0;
+  width: 100%;
+  max-width: 1000px;
   box-sizing: border-box;
   margin: 0 auto;
 `;
