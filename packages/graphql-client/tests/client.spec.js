3ece7b0bd368ae084c752a147f117857f96a2f1e
wangshijun <wangshijun2010@gmail.com>
2019-03-12 11:51:15 +0800

    chore: add test case for tx methods

--- packages/graphql-client/tests/client.spec.js
+++ packages/graphql-client/tests/client.spec.js
@@ -1,3 +1,6 @@
+const { fromRandom, WalletType } = require('@arcblock/forge-wallet');
+const Mcrypto = require('@arcblock/mcrypto');
+const { hexToBytes } = require('@arcblock/forge-util');
 const GraphqlClient = require('../src/client');
 
 describe('GraphqlClient', () => {
@@ -19,9 +22,38 @@ describe('GraphqlClient', () => {
     expect(client.getSubscriptions().length).toBeGreaterThan(0);
   });
 
-  test.skip('should support getBlock', async () => {
-    const { getBlock } = await client.getBlock({ height: 2 });
-    expect(getBlock.code).toEqual('OK');
-    expect(getBlock.block.height).toEqual(2);
+  test('should support getBlock', async () => {
+    const res = await client.getBlock({ height: 2 });
+    expect(res.code).toEqual('OK');
+    expect(res.block.height).toEqual(2);
+  });
+
+  test('should support getType', async () => {
+    const type = client.getType('Transaction');
+    expect(typeof type.fromObject).toEqual('function');
+    expect(typeof type.encode).toEqual('function');
+  });
+
+  test('should support declare account', async () => {
+    const type = WalletType({
+      role: Mcrypto.types.RoleType.ROLE_ACCOUNT,
+      pk: Mcrypto.types.KeyType.SECP256K1,
+      hash: Mcrypto.types.HashType.SHA3,
+    });
+
+    const wallet = fromRandom(type);
+    const res = await client.sendDeclareTx({
+      data: {
+        moniker: `wangshijun_${Math.round(Math.random() * 1000)}`,
+        pk: Buffer.from(hexToBytes(wallet.publicKey)),
+        type,
+        issuer: '',
+        data: null,
+      },
+      wallet,
+    });
+
+    expect(res.code).toEqual('OK');
+    expect(res.hash).toBeTruthy();
   });
 });
