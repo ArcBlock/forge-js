518d41c3ab8f075731b33ce8eb29b38b9852a01c
wangshijun <wangshijun2010@gmail.com>
2019-03-14 19:02:02 +0800

    fix: sparkline fill opacity in dark mode

--- apps/forge-web/src/components/sparkline.js
+++ apps/forge-web/src/components/sparkline.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { AreaChart, Area, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+import { useThemeMode } from '../libs/hooks';
+
 const renderTooltip = tip => {
   if (tip.payload[0] && tip.payload[0].payload) {
     const d = tip.payload[0].payload;
@@ -40,46 +42,49 @@ const TipContainer = styled.div`
   }
 `;
 
-const SparkLine = React.memo(({ data, series, legend }) => (
-  <ResponsiveContainer>
-    <AreaChart data={data} margin={{ top: 10, right: 3, left: 3, bottom: 10 }}>
-      <defs>
-        {series
-          .filter(x => x.gradientStart && x.gradientStop)
-          .map(({ dataKey, gradientStart, gradientStop }) => (
-            <linearGradient key={dataKey} id={`gradient-${dataKey}`} x1="0" y1="0" x2="0" y2="1">
-              <stop offset="0%" stopColor={gradientStart} stopOpacity={1} />
-              <stop offset="95%" stopColor={gradientStop} stopOpacity={0} />
-            </linearGradient>
-          ))}
-      </defs>
-      <Tooltip
-        cursor={false}
-        content={renderTooltip}
-        wrapperStyle={{
-          width: 'auto',
-          minWidth: '90px',
-          padding: '8px 12px',
-          fontSize: '14px',
-          color: '#ffffff',
-          backgroundColor: '#4a4a4a',
-          borderRadius: '2px',
-        }}
-      />
-      {legend && <Legend verticalAlign="top" height={36} />}
-      {series.map(({ dataKey, stroke, gradientStart, gradientStop }) => (
-        <Area
-          key={`area-${dataKey}`}
-          type="monotone"
-          dataKey={dataKey}
-          stroke={stroke}
-          fillOpacity={process.env.REACT_APP_NAME === 'explorer' ? 0.05 : 1}
-          fill={gradientStart && gradientStop ? `url(#gradient-${dataKey})` : null}
+const SparkLine = ({ data, series, legend }) => {
+  const [mode] = useThemeMode();
+  return (
+    <ResponsiveContainer>
+      <AreaChart data={data} margin={{ top: 10, right: 3, left: 3, bottom: 10 }}>
+        <defs>
+          {series
+            .filter(x => x.gradientStart && x.gradientStop)
+            .map(({ dataKey, gradientStart, gradientStop }) => (
+              <linearGradient key={dataKey} id={`gradient-${dataKey}`} x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor={gradientStart} stopOpacity={1} />
+                <stop offset="95%" stopColor={gradientStop} stopOpacity={0} />
+              </linearGradient>
+            ))}
+        </defs>
+        <Tooltip
+          cursor={false}
+          content={renderTooltip}
+          wrapperStyle={{
+            width: 'auto',
+            minWidth: '90px',
+            padding: '8px 12px',
+            fontSize: '14px',
+            color: '#ffffff',
+            backgroundColor: '#4a4a4a',
+            borderRadius: '2px',
+          }}
         />
-      ))}
-    </AreaChart>
-  </ResponsiveContainer>
-));
+        {legend && <Legend verticalAlign="top" height={36} />}
+        {series.map(({ dataKey, stroke, gradientStart, gradientStop }) => (
+          <Area
+            key={`area-${dataKey}`}
+            type="monotone"
+            dataKey={dataKey}
+            stroke={stroke}
+            fillOpacity={mode === 'dark' ? 0.05 : 1}
+            fill={gradientStart && gradientStop ? `url(#gradient-${dataKey})` : null}
+          />
+        ))}
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
 
 SparkLine.propTypes = {
   data: PropTypes.arrayOf(
