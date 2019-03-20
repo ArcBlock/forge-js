cded5ed394e462d12224c30f73e31373639787ac
wangshijun <wangshijun2010@gmail.com>
2019-03-20 11:57:39 +0800

    chore: add bundle analyzer and optimize recharts import

--- apps/forge-web/tools/config.js
+++ apps/forge-web/tools/config.js
@@ -1,8 +1,9 @@
 /* eslint no-unused-vars:"off" */
 const util = require('util');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const paths = require('./paths');
 
-const debug = (label, d) => console.log(label, util.inspect(d, { depth: 8, colors: true }));
+const debug = (label, d) => console.log(label, util.inspect(d, { depth: 9, colors: true }));
 
 module.exports = {
   webpack(config, env) {
@@ -13,12 +14,32 @@ module.exports = {
       type: 'javascript/auto',
     });
 
+    // Reduce recharts bundle size
+    const rule = config.module.rules.find(x => x.oneOf);
+    if (rule) {
+      const loaders = rule.oneOf;
+      const loader = loaders.find(
+        x => x.test && x.test.toString().indexOf('(js|mjs|jsx|ts|tsx)') > 0
+      );
+      loader.options.plugins.push(require.resolve('babel-plugin-recharts'));
+      debug('loader', loader);
+    }
+
     // Add entries
     const oldEntry = config.entry.slice(0, config.entry.length - 1);
     config.entry = oldEntry.concat([
       process.env.REACT_APP_NAME === 'explorer' ? paths.appExplorerJs : paths.appJs,
     ]);
 
+    if (env === 'production') {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          openAnalyzer: false,
+          generateStatsFile: true,
+        })
+      );
+    }
     // debug('webpack', { config, env });
     // process.exit(1);
 
