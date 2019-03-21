713a3199174975e4bf090ea8dee2a67e26d2a426
wangshijun <wangshijun2010@gmail.com>
2019-03-22 07:29:54 +0800

    fix: auth info generating

--- examples/nextjs-keystone-starter/client/pages/index.js
+++ examples/nextjs-keystone-starter/client/pages/index.js
@@ -9,17 +9,27 @@ export default function IndexPage() {
     <Layout>
       <Main>
         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-          Pricing
+          Your Wallet, The Digital Way
         </Typography>
         <Typography variant="h6" align="center" color="textSecondary" component="p">
-          Quickly build an effective pricing table for your potential customers with this layout.
-          It&apos;s built with default Material-UI components with little customization.
+          A decentralized wallet that puts you in control of everything - digital identity, profiles
+          and more. Find out who wants to see your data and manage what data you are willing to
+          share. Get notified anytime someone makes a request.
         </Typography>
+        <img
+          src="https://abt-wallet.netlify.com/static/5ab6559936f775f006a75eee19cce000/645bb/group-43.png"
+          alt=""
+        />
       </Main>
     </Layout>
   );
 }
 
 const Main = styled.main`
-  margin: 50px 0;
+  margin: 80px 0;
+  text-align: center;
+
+  img {
+    margin-top: 80px;
+  }
 `;
--- examples/nextjs-keystone-starter/server/routes/index.js
+++ examples/nextjs-keystone-starter/server/routes/index.js
@@ -1,33 +1,24 @@
-const keystone = require('keystone');
-const config = require('../../config/app');
+const auth = require('../../config/auth');
 
-// TODO: allow more flexible routes
 module.exports = nextApp => app => {
-  // Next request handler
   const handle = nextApp.getRequestHandler();
 
-  app.get('/api/posts', (req, res) => {
-    const Post = keystone.list('Post');
-    Post.model
-      .find()
-      .where('state', 'published')
-      .sort('-publishedDate')
-      .exec((err, results) => {
-        if (err) throw err;
-        res.json(results);
-      });
-  });
-
   app.get('/api/session', (req, res) => {
-    res.jsonp(req.session);
+    res.json(req.session);
   });
 
   app.get('/api/login', (req, res) => {
-    res.send(config.getLoginUri());
+    res.send(auth.getLoginUri());
   });
 
   app.get('/api/auth', (req, res) => {
-    res.jsonp(config.getAuthInfo(req.query.userDid));
+    const authInfo = auth.getAuthInfo(req.query.userDid);
+    console.log('requestAuth', { query: req.query, authInfo });
+    res.json(authInfo);
+  });
+
+  app.post('/api/auth', (req, res) => {
+    res.json(auth.verifyAuth(req.body));
   });
 
   app.get('*', (req, res) => handle(req, res));
