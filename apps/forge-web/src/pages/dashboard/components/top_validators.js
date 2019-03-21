4c810d6e00ae288874e7ffab7e0fac21a8d341d0
wangshijun <wangshijun2010@gmail.com>
2019-03-21 15:07:00 +0800

    chore: add pagination for top validators

--- apps/forge-web/src/pages/dashboard/components/top_validators.js
+++ apps/forge-web/src/pages/dashboard/components/top_validators.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useAsync } from 'react-use';
 import { Link } from 'react-router-dom';
@@ -11,6 +11,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 import AccountActivity from './account_activity';
+import Pagination from '../../../components/pagination';
+
 import forge from '../../../libs/forge';
 import { getExplorerUrl } from '../../../libs/util';
 
@@ -21,8 +23,11 @@ async function fetchTopValidators() {
   return validators.sort((a, b) => b.votingPower - a.votingPower);
 }
 
+const pageSize = 10;
+
 export default function TopValidators({ sparkline }) {
   const state = useAsync(fetchTopValidators);
+  const [currentPage, setCurrentPage] = useState(1);
 
   if (state.loading) {
     return <CircularProgress />;
@@ -32,39 +37,61 @@ export default function TopValidators({ sparkline }) {
     return <p className="error">{state.error.message}</p>;
   }
 
+  const renderPagination = () => {
+    if (state.value.length > pageSize) {
+      return (
+        <Pagination
+          onChange={setCurrentPage}
+          pageSize={pageSize}
+          current={currentPage}
+          total={state.value.length}
+          className="pagination"
+        />
+      );
+    }
+
+    return null;
+  };
+
+  const offset = (currentPage - 1) * pageSize;
+  const validators = state.value.slice(offset, currentPage * pageSize);
+
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell align="left">Rank</TableCell>
-          <TableCell align="left">Address</TableCell>
-          <TableCell align="center">Voting Power</TableCell>
-          <TableCell align="center">Proposer Priority</TableCell>
-          {sparkline && (
-            <TableCell align="left" style={{ width: '25%' }}>
-              Activities
-            </TableCell>
-          )}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {state.value.map((x, i) => (
-          <TableRow key={x.address}>
-            <TableCell align="left">{i + 1}</TableCell>
-            <TableCell align="left">
-              <Link to={getExplorerUrl(`/accounts/${x.address}`)}>{x.address}</Link>
-            </TableCell>
-            <TableCell align="center">{x.votingPower}</TableCell>
-            <TableCell align="center">{x.proposerPriority}</TableCell>
+    <React.Fragment>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Rank</TableCell>
+            <TableCell align="left">Address</TableCell>
+            <TableCell align="center">Voting Power</TableCell>
+            <TableCell align="center">Proposer Priority</TableCell>
             {sparkline && (
-              <TableCell align="left">
-                <AccountActivity data={[...Array(7).fill(0)]} delayMS={i * 500} />
+              <TableCell align="left" style={{ width: '25%' }}>
+                Activities
               </TableCell>
             )}
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {validators.map((x, i) => (
+            <TableRow key={x.address}>
+              <TableCell align="left">{offset + i + 1}</TableCell>
+              <TableCell align="left">
+                <Link to={getExplorerUrl(`/accounts/${x.address}`)}>{x.address}</Link>
+              </TableCell>
+              <TableCell align="center">{x.votingPower}</TableCell>
+              <TableCell align="center">{x.proposerPriority}</TableCell>
+              {sparkline && (
+                <TableCell align="left">
+                  <AccountActivity data={[...Array(7).fill(0)]} delayMS={i * 500} />
+                </TableCell>
+              )}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {renderPagination()}
+    </React.Fragment>
   );
 }
 
