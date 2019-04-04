0b5a13c2f7ecb146daef0a348036de855ad6b1de
wangshijun <wangshijun2010@gmail.com>
2019-04-04 18:42:28 +0800

    fix: date nan on ios

--- apps/forge-web/src/libs/dayjs.js
+++ apps/forge-web/src/libs/dayjs.js
@@ -8,7 +8,11 @@ dayjs.extend(relative);
 // @link https://github.com/iamkun/dayjs/issues/254#issuecomment-415278024
 const fix = (date, c) => {
   if (typeof date === 'string') {
-    return dayjs(date.replace(/-/g, '/'), c);
+    const [dateStr, _] = date.split(/\s/);
+    const [timeStr] = _.split('.');
+    const str = `${dateStr.replace(/-/g, '/')} ${timeStr}`;
+
+    return dayjs(str, c);
   }
   return dayjs(date, c);
 };
