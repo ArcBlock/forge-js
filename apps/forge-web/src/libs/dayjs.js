56d47ac14b6c2f27b4327fec647c8c16516d1058
wangshijun <wangshijun2010@gmail.com>
2019-04-06 10:03:03 +0800

    fix: date parsing in ios safari

--- apps/forge-web/src/libs/dayjs.js
+++ apps/forge-web/src/libs/dayjs.js
@@ -5,22 +5,41 @@ import relative from 'dayjs/plugin/relativeTime';
 dayjs.extend(utc);
 dayjs.extend(relative);
 
+function dateFromString(str) {
+  const isUTC = /Z$/.test(str);
+  const ints = str.split(/[^0-9]/).map(s => parseInt(s, 10));
+  if (isUTC) {
+    const ms = Date.UTC(
+      ints[0],
+      ints[1] - 1 || 0,
+      ints[2] || 1,
+      ints[3] || 0,
+      ints[4] || 0,
+      ints[5] || 0,
+      ints[6] || 0
+    );
+
+    return new Date(ms);
+  }
+  return new Date(
+    ints[0],
+    ints[1] - 1 || 0,
+    ints[2] || 1,
+    ints[3] || 0,
+    ints[4] || 0,
+    ints[5] || 0,
+    ints[6] || 0
+  );
+}
+
 // @link https://github.com/iamkun/dayjs/issues/254#issuecomment-415278024
 // @link https://stackoverflow.com/questions/9706688/what-does-the-z-mean-in-unix-timestamp-120314170138z
 const fix = (date, c) => {
   let isUTC = false;
   if (typeof date === 'string') {
     isUTC = /Z$/.test(date);
-
-    const [dateStr, _] = date
-      .replace(/T/g, ' ')
-      .replace(/Z$/, '')
-      .split(/\s/);
-
-    const [timeStr] = _ ? _.split('.') : [''];
-
     // eslint-disable-next-line no-param-reassign
-    date = `${dateStr.replace(/-/g, '/')} ${timeStr}`.trim();
+    date = dateFromString(date);
   }
 
   console.log({ isUTC, date });
