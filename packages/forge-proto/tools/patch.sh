6973f975270776145c4c12fb70764d560536dc16
wangshijun <wangshijun2010@gmail.com>
2019-04-09 15:28:47 +0800

    chore: update forge-proto

--- packages/forge-proto/tools/patch.sh
+++ packages/forge-proto/tools/patch.sh
@@ -2,7 +2,7 @@
 for FILE in lib/*_pb.js
 do
   sed -i -E "s/get([a-zA-Z0-9]+)_asB64\(\)/get\1()/" $FILE
-  sed -i -E "s/new\sBuffer\(/Buffer.from(/" $FILE
+  # sed -i -E "s/new\sBuffer\(/Buffer.from(/" $FILE
   # sed -i -E "s/get([a-zA-Z0-9]+)_asU8\(\)/get\1()/" $FILE
   if [[ "$FILE" =~ "_grpc_pb" ]]; then
     echo "skip: $FILE"
