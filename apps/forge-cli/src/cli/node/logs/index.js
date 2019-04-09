b44873d0b9e7ef58bda6fee18a703624060519e4
wangshijun <wangshijun2010@gmail.com>
2019-04-10 06:23:46 +0800

    fix: forge logs command ignored by git

---
+++ apps/forge-cli/src/cli/node/logs/index.js
@@ -0,0 +1,26 @@
+const shell = require('shelljs');
+const chalk = require('chalk');
+const { cli, action } = require('core/cli');
+const { execute, run } = require('./logs');
+
+cli('logs [type]', 'Show logs for various forge components', input => action(execute, run, input), {
+  requirements: {
+    forgeRelease: true,
+    runningNode: false,
+    rpcClient: true,
+    wallet: false,
+  },
+  options: [],
+  handlers: {
+    '--help': () => {
+      shell.echo(`
+Examples:
+  - ${chalk.cyan('forge logs')}               display all logs
+  - ${chalk.cyan('forge logs ipfs')}          display ipfs logs
+  - ${chalk.cyan('forge logs app')}           display app logs
+  - ${chalk.cyan('forge logs core')}          display forge core logs
+  - ${chalk.cyan('forge logs tendermint')}    display tendermint logs
+        `);
+    },
+  },
+});
