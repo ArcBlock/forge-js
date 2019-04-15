1e792829354141a3456335faaa3b835e443474ad
wangshijun <wangshijun2010@gmail.com>
2019-04-15 18:20:01 +0800

    chore: add more test case for forge-web

--- apps/forge-web/src/setupTests.js
+++ apps/forge-web/src/setupTests.js
@@ -2,6 +2,8 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-localstorage-mock';
 
+process.env.REACT_APP_NAME = 'chain_node_web';
+
 global.document.body.createRange = () => ({
   setEnd: () => {},
   setStart: () => {},
