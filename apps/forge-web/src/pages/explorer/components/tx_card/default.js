b3ac09a01503727109585256c4a99a31d2539bd0
wangshijun <wangshijun2010@gmail.com>
2019-04-22 18:36:07 +0800

    feat: support upgrade node tx in tx card

--- apps/forge-web/src/pages/explorer/components/tx_card/default.js
+++ apps/forge-web/src/pages/explorer/components/tx_card/default.js
@@ -1,19 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-// import dayjs from 'dayjs';
-// import Typography from '@material-ui/core/Typography';
+
+import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+
+import { getExplorerUrl } from '../../../../libs/util';
+import dayjs from '../../../../libs/dayjs';
 
 const DefaultTxSummary = React.memo(({ tx, ...rest }) => (
   <Container {...rest}>
-    <h3>Default Summary</h3>
-    <pre>
-      <code>{JSON.stringify(tx.tx, true, '  ')}</code>
-    </pre>
+    <div className="info-row info-row--full">
+      <Typography component="p" className="hash" title={tx.hash}>
+        <Link to={getExplorerUrl(`/txs/${tx.hash}`)}># {tx.hash}</Link>
+      </Typography>
+      <Typography component="p" className="time" title={tx.time}>
+        {dayjs(tx.time).fromNow()}
+      </Typography>
+    </div>
+    <div className="info-row">
+      <pre>
+        <code>{JSON.stringify(tx, true, '  ')}</code>
+      </pre>
+    </div>
   </Container>
 ));
 
-const Container = styled.div``;
+const Container = styled.div`
+  .info-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    margin-bottom: 10px;
+
+    &:nth-last-of-type() {
+      margin-bottom: 0;
+    }
+
+    a {
+      padding-bottom: 2px;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .info-row--full {
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .time,
+  .hash a {
+    color: ${props => props.theme.typography.color.gray};
+    font-size: 12px;
+  }
+`;
 
 DefaultTxSummary.propTypes = {
   tx: PropTypes.object.isRequired,
