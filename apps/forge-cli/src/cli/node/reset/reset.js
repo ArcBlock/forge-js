6fba3acf1b2834a9bc4863bb362ca5c0a53bb4dd
wangshijun <wangshijun2010@gmail.com>
2019-04-27 06:25:11 +0800

    support forge reset (#247)
    * chore: add forge reset sub command
    
    * chore: update dependency version
    
    * chore: bump version

---
+++ apps/forge-cli/src/cli/node/reset/reset.js
@@ -0,0 +1,47 @@
+const shell = require('shelljs');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const { config } = require('core/env');
+const { symbols } = require('core/ui');
+
+async function main() {
+  // Confirm stopped
+  const { starterBinPath, forgeConfigPath } = config.get('cli');
+  const { stdout: pid } = shell.exec(`FORGE_CONFIG=${forgeConfigPath} ${starterBinPath} pid`, {
+    silent: true,
+  });
+
+  const pidNumber = Number(pid);
+  if (pidNumber) {
+    shell.echo(`${symbols.error} forge is running!`);
+    shell.echo(`${symbols.info} Please run ${chalk.cyan('forge stop')} first, then we can reset!`);
+    process.exit(0);
+    return;
+  }
+
+  // Confirm
+  const questions = [
+    {
+      type: 'confirm',
+      name: 'confirm',
+      default: false,
+      message: chalk.red(
+        'Reset chain state will erase all local data, including transactions/logs, are you sure to continue?'
+      ),
+    },
+  ];
+  const { confirm } = await inquirer.prompt(questions);
+  if (confirm) {
+    shell.exec('rm -rf ~/.forge_release');
+    shell.echo(`${symbols.info} rm -rf ~/.forge_release`);
+    shell.exec('rm -rf ~/.forge_cli/keys');
+    shell.echo(`${symbols.info} rm -rf ~/.forge_cli/keys`);
+  } else {
+    shell.echo(`${symbols.info} User abort, nothing changed!`);
+    process.exit();
+    return;
+  }
+}
+
+exports.run = main;
+exports.execute = main;
