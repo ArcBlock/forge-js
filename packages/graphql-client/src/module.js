49e41f71f186a1fc9782008730a58f1f6502852a
wangshijun <wangshijun2010@gmail.com>
2019-04-29 14:32:17 +0800

    chore: add module placeholders for jsdoc

---
+++ packages/graphql-client/src/module.js
@@ -0,0 +1,9 @@
+/**
+ * @fileOverview Defines the most important client library that can connect an app with forge powered blockchain: {@link GraphQLClient}
+ *
+ * GraphQLClient can be used both in node.js or in the browser, if you are looking for a client library for backend javascript applications, please checkout {@link GRpcClient}
+ *
+ * @module @arcblock/graphql-client
+ * @see GraphQLClient
+ * @see GRpcClient
+ */
---
+++ packages/grpc-client/lib/module.js
@@ -0,0 +1,9 @@
+/**
+ * @fileOverview Defines the most efficient client library that can connect an app with forge powered blockchain through gRPC: {@link GRpcClient}
+ *
+ * GRpcClient can only be used in node.js, and is more efficient thant {@link GraphQLClient} which connect your app with forge node through http endpoint.
+ *
+ * @module @arcblock/grpc-client
+ * @see GRpcClient
+ * @see GraphQLClient
+ */
