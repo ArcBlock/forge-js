c657e102abb0c4dfb5cb5d03ef555b4818e63ee7
wangshijun <wangshijun2010@gmail.com>
2019-04-30 07:27:26 +0800

    chore: imporve mcrypto docs

--- packages/mcrypto/lib/index.js
+++ packages/mcrypto/lib/index.js
@@ -15,39 +15,6 @@
 yarn add @arcblock/mcrypto
  */
 
-// FIXME: enum definition of forge-abi and abt-did-elixir are not exactly the same
-const types = {
-  KeyType: {
-    ED25519: 0,
-    SECP256K1: 1,
-  },
-  HashType: {
-    KECCAK: 0,
-    SHA3: 1,
-    KECCAK_384: 2,
-    SHA3_384: 3,
-    KECCAK_512: 4,
-    SHA3_512: 5,
-  },
-  RoleType: {
-    ROLE_ACCOUNT: 0,
-    ROLE_NODE: 1,
-    ROLE_DEVICE: 2,
-    ROLE_APPLICATION: 3,
-    ROLE_SMART_CONTRACT: 4,
-    ROLE_BOT: 5,
-    ROLE_ASSET: 6,
-    ROLE_STAKE: 7,
-    ROLE_VALIDATOR: 8,
-    ROLE_GROUP: 9,
-    ROLE_ANY: 63,
-  },
-  EncodingType: {
-    BASE16: 0,
-    BASE58: 1,
-  },
-};
-
 const Mcrypto = (module.exports = {
   /**
    * Contains all supported signers, eg: `Ed25519` and `Secp256k1`
@@ -56,6 +23,15 @@ const Mcrypto = (module.exports = {
    * @type {object}
    * @name Signer
    * @static
+   * @example
+   * const { Signer } = require('@arcblock/mcrypto');
+   * const message = 'some message to sign';
+   *
+   * // Use Signer directly
+   * const keyPair = Signer.Ed25519.genKeyPair();
+   * const signature = Signer.Ed25519.sign(message, keyPair.secretKey);
+   * const result = Signer.Ed25519.verify(message, signature, keyPair.publicKey);
+   * assert.ok(result);
    */
   Signer: {
     Ed25519: require('./signer/ed25519'),
@@ -69,6 +45,11 @@ const Mcrypto = (module.exports = {
    * @type {object}
    * @name Hasher
    * @static
+   * @example
+   * const { Hasher } = require('@arcblock/mcrypto');
+   *
+   * const message = 'message to hash';
+   * const hash = Hasher.SHA2.hash256(message);
    */
   Hasher: {
     SHA2: require('./hasher/sha2'),
@@ -88,16 +69,87 @@ const Mcrypto = (module.exports = {
 
   /**
    * Contains type constants that represent can be used to compose different crypto method, each crypto method consist one of:
-   *
-   * - Signer
-   * - Hahser
+   * FIXME: enum definition of forge-abi and abt-did-elixir are not exactly the same
    *
    * @readonly
    * @type {object}
    * @name types
    * @static
+   * @example
+   * const { types } = require('@arcblock/mcrypto');
+   *
+   * // types.RoleType.ROLE_ACCOUNT
+   * // types.KeyType.ED25519
+   * // types.HashType.SHA3
+   * // types.EncodingType.BASE58
    */
-  types,
+  types: {
+    /**
+     * Key-pair derivation algorithms
+     *
+     * @readonly
+     * @enum {number}
+     * @name types.KeyType
+     * @memberof types
+     * @static
+     */
+    KeyType: {
+      ED25519: 0,
+      SECP256K1: 1,
+    },
+    /**
+     * Hashing algorithms
+     *
+     * @readonly
+     * @enum {number}
+     * @name types.HashType
+     * @memberof types
+     * @static
+     */
+    HashType: {
+      KECCAK: 0,
+      SHA3: 1,
+      KECCAK_384: 2,
+      SHA3_384: 3,
+      KECCAK_512: 4,
+      SHA3_512: 5,
+    },
+    /**
+     * DID wallet role type
+     *
+     * @readonly
+     * @enum {number}
+     * @name types.RoleType
+     * @memberof types
+     * @static
+     */
+    RoleType: {
+      ROLE_ACCOUNT: 0,
+      ROLE_NODE: 1,
+      ROLE_DEVICE: 2,
+      ROLE_APPLICATION: 3,
+      ROLE_SMART_CONTRACT: 4,
+      ROLE_BOT: 5,
+      ROLE_ASSET: 6,
+      ROLE_STAKE: 7,
+      ROLE_VALIDATOR: 8,
+      ROLE_GROUP: 9,
+      ROLE_ANY: 63,
+    },
+    /**
+     * Address encoding algorithm, defaults to `base58btc`
+     *
+     * @readonly
+     * @enum {number}
+     * @name types.RoleType
+     * @memberof types
+     * @static
+     */
+    EncodingType: {
+      BASE16: 0,
+      BASE58: 1,
+    },
+  },
 
   /**
    * Get signer instance
@@ -108,16 +160,9 @@ const Mcrypto = (module.exports = {
    * - types.KeyType.SECP256k1
    * @returns {object} signer instance
    * @example
-   * const { Signer, getSigner, types } = require('@arcblock/mcrypto');
+   * const { getSigner, types } = require('@arcblock/mcrypto');
    * const message = 'some message to sign';
    *
-   * // Use Signer directly
-   * const keyPair = Signer.Ed25519.genKeyPair();
-   * const signature = Signer.Ed25519.sign(message, keyPair.secretKey);
-   * const result = Signer.Ed25519.verify(message, signature, keyPair.publicKey);
-   * assert.ok(result);
-   *
-   * // Get signer on fly
    * const signer = getSigner(types.KeyType.ED25519);
    * const keyPair1 = signer.genKeyPair();
    * const signature1 = signer.sign(message, keyPair1.secretKey);
@@ -145,13 +190,8 @@ const Mcrypto = (module.exports = {
    * - types.HashType.SHA3_512
    * @returns {object} hasher instance
    * @example
-   * const { Hasher, getHasher, types } = require('@arcblock/mcrypto');
+   * const { getHasher, types } = require('@arcblock/mcrypto');
    *
-   * // Choose from Hasher
-   * const message = 'message to hash';
-   * const hash = Hasher.SHA2.hash256(message);
-   *
-   * // user getHasher
    * const hashFn = getHasher(types.HashType.SHA3);
    * const hash2 = hashFn(message);
    */
@@ -165,15 +205,15 @@ const Mcrypto = (module.exports = {
 });
 
 const Signers = Object.freeze({
-  [types.KeyType.ED25519]: Mcrypto.Signer.Ed25519,
-  [types.KeyType.SECP256K1]: Mcrypto.Signer.Secp256k1,
+  [Mcrypto.types.KeyType.ED25519]: Mcrypto.Signer.Ed25519,
+  [Mcrypto.types.KeyType.SECP256K1]: Mcrypto.Signer.Secp256k1,
 });
 
 const Hashers = Object.freeze({
-  [types.HashType.KECCAK]: Mcrypto.Hasher.Keccak.hash256,
-  [types.HashType.KECCAK_384]: Mcrypto.Hasher.Keccak.hash384,
-  [types.HashType.KECCAK_512]: Mcrypto.Hasher.Keccak.hash512,
-  [types.HashType.SHA3]: Mcrypto.Hasher.SHA3.hash256,
-  [types.HashType.SHA3_384]: Mcrypto.Hasher.SHA3.hash384,
-  [types.HashType.SHA3_512]: Mcrypto.Hasher.SHA3.hash512,
+  [Mcrypto.types.HashType.KECCAK]: Mcrypto.Hasher.Keccak.hash256,
+  [Mcrypto.types.HashType.KECCAK_384]: Mcrypto.Hasher.Keccak.hash384,
+  [Mcrypto.types.HashType.KECCAK_512]: Mcrypto.Hasher.Keccak.hash512,
+  [Mcrypto.types.HashType.SHA3]: Mcrypto.Hasher.SHA3.hash256,
+  [Mcrypto.types.HashType.SHA3_384]: Mcrypto.Hasher.SHA3.hash384,
+  [Mcrypto.types.HashType.SHA3_512]: Mcrypto.Hasher.SHA3.hash512,
 });
