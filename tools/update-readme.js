d03dad18395d30a202c10850eb08ea1f2bd7a9b1
wangshijun <wangshijun2010@gmail.com>
2019-05-07 14:01:01 +0800

    chore: update readme

--- tools/update-readme.js
+++ tools/update-readme.js
@@ -24,12 +24,27 @@ const readmeFile = path.join(__dirname, '../README.md');
 const readmeContent = `# forge-js
 
 [![lerna](https://img.shields.io/badge/maintained%20with-lerna-cc00ff.svg)](https://lernajs.io/)
+[![docs](https://img.shields.io/badge/powered%20by-arcblock-green.svg)](https://docs.arcblock.io)
 
-> Javascript SDK for forge, an awesome framework to write distributed blockchain applications, last updated at ${new Date().toLocaleString()}
+> Javascript SDK for [forge](https://docs.arcblock.io/forge/latest/), which is an awesome framework to write distributed blockchain applications.
+
+> Last updated at ${new Date().toLocaleString()}
 
 ## Packages Included
 
 ${packageList.concat(appList).join('\n')}
+
+## Documentation
+
+[https://docs.arcblock.io/forge/sdks/javascript/latest/](https://docs.arcblock.io/forge/sdks/javascript/latest/)
+
+## Contribution
+
+Checkout [CONTRIBUTION.md](./CONTRIBUTION.md)
+
+## Report a Bug?
+
+Bugs and feature requests please create new issues [here](/issues)
 `;
 
 fs.writeFileSync(readmeFile, readmeContent);
