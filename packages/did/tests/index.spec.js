5461b212cf533bc528e7c8630aa6389fdf6147dd
wangshijun <wangshijun2010@gmail.com>
2019-05-21 14:14:11 +0800

    chore: add testcase for jwtVerify

--- packages/did/tests/index.spec.js
+++ packages/did/tests/index.spec.js
@@ -1,4 +1,6 @@
+const multibase = require('multibase');
 const base64 = require('base64-url');
+const { bytesToHex } = require('@arcblock/forge-util');
 const Mcrypto = require('@arcblock/mcrypto');
 const {
   types,
@@ -149,6 +151,15 @@ describe('@arcblock/did', () => {
     const pk = '0xE4852B7091317E3622068E62A5127D1FB0D4AE2FC50213295E10652D2F0ABFC7';
     const token =
       'eyJhbGciOiJFZDI1NTE5IiwidHlwIjoiSldUIn0.eyJrZXkiOiJ2YWx1ZSIsImlzcyI6ImRpZDphYnQ6ek5LdENOcVlXTFlXWVczZ1dSQTF2blJ5a2ZDQlpZSFp2ektyIn0.5cP-B1SCbPUZsq9NeFSRyRXBncvljUrowHd6EWCPrK-LP2j7pHQb1j9h2ZdaU2435HXL_EYChN7teilTa6xYDA';
+    const result = jwtVerify(token, pk);
+    expect(result).toEqual(true);
+  });
+
+  // FIXME: this signature is valid in both java and elixir
+  it.skip('should be able to verify jwt token signed by java', () => {
+    const pk = bytesToHex(multibase.decode('z2wxiRgf3fm2hmBGPJRbhLhMhYLzU5UviMitDnpELJz9a'));
+    const token =
+      'eyJhbGciOiJFZDI1NTE5IiwidHlwIjoiSldUIn0.eyJleHAiOiIxNTU4MzQ1NDExIiwiaWF0IjoiMTU1ODM0NTM1MSIsImlzcyI6ImRpZDphYnQ6ejFiWVhqWWR0ZE52MlZoM0doSGlFYlg1ZkRmc2JiV1NjWlgiLCJuYmYiOiIxNTU4MzQ1MzUxIiwicmVxdWVzdGVkQ2xhaW1zIjpbeyJ0eXBlIjoicHJvZmlsZSIsImZ1bGxOYW1lIjoiYnVkZHkiLCJlbWFpbCI6IjE4NDYxMzIzM0BxcS5jb20ifV19.HxPj8bbKFYDSK_rdRe2BdK_7YJ_cP-MLiXOVqqUlF34hTlwLAjgHXZs5z6EiASFCDLz2Q622JbJbsiLKz558Cg';
     const result = jwtVerify(token, pk);
     expect(result).toEqual(true);
   });
