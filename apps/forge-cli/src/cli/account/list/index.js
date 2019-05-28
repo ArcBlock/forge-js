e0ea16dded7702c254d1622945162a67ca7371e2
wangshijun <wangshijun2010@gmail.com>
2019-05-28 11:58:36 +0800

    fix: eslint errors in tcp-server

--- apps/forge-cli/src/cli/account/list/index.js
+++ apps/forge-cli/src/cli/account/list/index.js
@@ -1,13 +1,10 @@
 // eslint-disable-next-line import/no-unresolved
 const { cli, action } = require('core/cli');
-const { enums } = require('@arcblock/forge-proto');
 const { execute, run } = require('./list');
-
-const roles = ['all'].concat(Object.keys(enums.AccountRole).map(x => x.toLowerCase()));
 
 cli(
   'account:list [role]',
-  `List all accounts stored in this node, role=[${roles}], default role is all`,
+  'List all accounts stored in this node',
   input => action(execute, run, input),
   {
     requirements: {
