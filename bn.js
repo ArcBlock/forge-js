53006e7eb065a8db9903663d3b9226b5a141352a
wangshijun <wangshijun2010@gmail.com>
2019-06-28 18:24:57 +0800

    chore: remove useless file

--- bn.js
+++
@@ -1,4 +0,0 @@
-const { toBN, fromTokenToUnit, toHex } = require('@arcblock/forge-util');
-
-console.log(toBN('35C9ADC5DEA00000').toString());
-console.log(toHex(fromTokenToUnit(10000, 16).toBuffer()));
