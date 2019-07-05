4292f3a4fbaa9b0b09555552d6efa6da56e9c69e
wangshijun <wangshijun2010@gmail.com>
2019-07-05 15:35:44 +0800

    chore: upgrade forge-proto with forge v0.32.2

--- packages/forge-proto/proto-lite.js
+++ packages/forge-proto/proto-lite.js
@@ -1,47 +1,53 @@
-// Auto generated code (2019-06-07T07:22:07.446Z), DO NOT EDIT
-const typePb = require('./lib/type_pb.js');
+// Auto generated code (2019-07-05T07:33:04.809Z), DO NOT EDIT
 const enumPb = require('./lib/enum_pb.js');
-const withdrawTetherTxPb = require('./lib/withdraw_tether_tx_pb.js');
-const declareTxPb = require('./lib/declare_tx_pb.js');
+const typePb = require('./lib/type_pb.js');
 const accountMigrateTxPb = require('./lib/account_migrate_tx_pb.js');
-const transferTxPb = require('./lib/transfer_tx_pb.js');
-const exchangeTxPb = require('./lib/exchange_tx_pb.js');
-const createAssetTxPb = require('./lib/create_asset_tx_pb.js');
-const revokeTetherTxPb = require('./lib/revoke_tether_tx_pb.js');
-const exchangeTetherTxPb = require('./lib/exchange_tether_tx_pb.js');
-const approveTetherTxPb = require('./lib/approve_tether_tx_pb.js');
-const upgradeNodeTxPb = require('./lib/upgrade_node_tx_pb.js');
-const updateAssetTxPb = require('./lib/update_asset_tx_pb.js');
-const consumeAssetTxPb = require('./lib/consume_asset_tx_pb.js');
-const depositTetherTxPb = require('./lib/deposit_tether_tx_pb.js');
 const acquireAssetTxPb = require('./lib/acquire_asset_tx_pb.js');
-const pokeTxPb = require('./lib/poke_tx_pb.js');
+const approveTetherTxPb = require('./lib/approve_tether_tx_pb.js');
+const consumeAssetTxPb = require('./lib/consume_asset_tx_pb.js');
+const createAssetTxPb = require('./lib/create_asset_tx_pb.js');
+const declareTxPb = require('./lib/declare_tx_pb.js');
 const deployProtocolTxPb = require('./lib/deploy_protocol_tx_pb.js');
+const depositTetherTxPb = require('./lib/deposit_tether_tx_pb.js');
+const exchangeTetherTxPb = require('./lib/exchange_tether_tx_pb.js');
+const exchangeTxPb = require('./lib/exchange_tx_pb.js');
+const pokeTxPb = require('./lib/poke_tx_pb.js');
+const retrieveSwapTxPb = require('./lib/retrieve_swap_tx_pb.js');
+const revokeSwapTxPb = require('./lib/revoke_swap_tx_pb.js');
+const revokeTetherTxPb = require('./lib/revoke_tether_tx_pb.js');
+const setupSwapTxPb = require('./lib/setup_swap_tx_pb.js');
 const stakeTxPb = require('./lib/stake_tx_pb.js');
+const transferTxPb = require('./lib/transfer_tx_pb.js');
+const updateAssetTxPb = require('./lib/update_asset_tx_pb.js');
+const upgradeNodeTxPb = require('./lib/upgrade_node_tx_pb.js');
+const withdrawTetherTxPb = require('./lib/withdraw_tether_tx_pb.js');
 
 const vendorPb = require('./lib/vendor_pb.js');
 
 const forgeTypes = Object.assign(
   {},
-  typePb,
   enumPb,
-  withdrawTetherTxPb,
-  declareTxPb,
+  typePb,
   accountMigrateTxPb,
-  transferTxPb,
-  exchangeTxPb,
-  createAssetTxPb,
-  revokeTetherTxPb,
-  exchangeTetherTxPb,
-  approveTetherTxPb,
-  upgradeNodeTxPb,
-  updateAssetTxPb,
-  consumeAssetTxPb,
-  depositTetherTxPb,
   acquireAssetTxPb,
-  pokeTxPb,
+  approveTetherTxPb,
+  consumeAssetTxPb,
+  createAssetTxPb,
+  declareTxPb,
   deployProtocolTxPb,
-  stakeTxPb
+  depositTetherTxPb,
+  exchangeTetherTxPb,
+  exchangeTxPb,
+  pokeTxPb,
+  retrieveSwapTxPb,
+  revokeSwapTxPb,
+  revokeTetherTxPb,
+  setupSwapTxPb,
+  stakeTxPb,
+  transferTxPb,
+  updateAssetTxPb,
+  upgradeNodeTxPb,
+  withdrawTetherTxPb
 );
 const forgeServices = {};
 const vendorTypes = Object.assign({}, vendorPb);
