28404c9688e56bfa5f87a0adb3e9a06a12136b4b
wangshijun <wangshijun2010@gmail.com>
2019-07-15 16:30:36 +0800

    fix: simple publish script not call cnpm sync script

--- tools/simple-publish.sh
+++ tools/simple-publish.sh
@@ -5,6 +5,9 @@ VERSION=$(cat version | awk '{$1=$1;print}')
 lerna run build
 lerna publish $VERSION --yes
 
+# trigger cnpm sync
+node tools/post-publish.js
+
 # update readme
 node tools/update-readme.js
 git commit -nam 'update readme'
