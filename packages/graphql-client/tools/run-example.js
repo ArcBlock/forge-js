ba1c6c0ba3167fc2eddbf21cbbb5e7b450693625
wangshijun <wangshijun2010@gmail.com>
2019-07-19 11:48:36 +0800

    fix: example running script

--- packages/graphql-client/tools/run-example.js
+++ packages/graphql-client/tools/run-example.js
@@ -5,12 +5,15 @@ const shell = require('shelljs');
 
 const examples = glob.sync('../examples/*.js', { cwd: __dirname }).filter(x => x.endsWith('.js'));
 examples.forEach(x => {
+  if (x.includes('subscribe')) {
+    return;
+  }
   const example = path.resolve(__dirname, x);
   console.log('');
   console.log('-'.repeat(80));
   console.log('Running example', example);
   console.log('-'.repeat(80));
-  shell.exec(`DEBUG=@arcblock/* FORGE_API_HOST=${process.env.FORGE_API_HOST} node ${example}`);
+  shell.exec(`DEBUG=@arcblock/* node ${example}`);
   console.log('');
   console.log('');
 });
