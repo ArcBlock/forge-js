dd56bdb19721e543e04f3debea49efb8a563adb4
wangshijun <wangshijun2010@gmail.com>
2019-11-06 10:46:09 +0800

    feat: support createAssetFactory and acquireAsset shortcut methods

--- examples/contract/asset_factory.js
+++ examples/contract/asset_factory.js
@@ -1,132 +1,77 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-console */
-require('./.compiled/create_product/javascript/index');
+require('./contract/.compiled/create_product/javascript/index');
 
-const moment = require('moment');
-const Mcrypto = require('@arcblock/mcrypto');
 const GraphqlClient = require('@arcblock/graphql-client');
-const { toAssetAddress } = require('@arcblock/did-util');
-const { fromTokenToUnit } = require('@arcblock/forge-util');
-const { fromRandom, WalletType } = require('@arcblock/forge-wallet');
+const { fromRandom } = require('@arcblock/forge-wallet');
 
 const endpoint = process.env.FORGE_API_HOST || 'http://127.0.0.1:8210'; // testnet
 
 const client = new GraphqlClient(`${endpoint}/api`);
 const sleep = timeout => new Promise(resolve => setTimeout(resolve, timeout));
 
-const type = WalletType({
-  role: Mcrypto.types.RoleType.ROLE_ACCOUNT,
-  pk: Mcrypto.types.KeyType.ED25519,
-  hash: Mcrypto.types.HashType.SHA3,
-});
-
 (async () => {
   try {
-    const merchant = fromRandom(type);
-    const customer = fromRandom(type);
-    console.log({ merchant: merchant.toJSON(), customer: customer.toJSON() });
+    const merchant = fromRandom();
+    const customer = fromRandom();
+    console.log({ merchant: merchant.toAddress(), customer: customer.toAddress() });
 
     // 1. declare merchant
-    let res = await client.sendDeclareTx({
-      tx: { itx: { moniker: 'merchant' } },
-      wallet: merchant,
-    });
-    console.log('merchant declare tx', res);
+    let hash = await client.declare({ moniker: 'merchant', wallet: merchant });
+    console.log('merchant declare tx', hash);
     console.log('merchant view link', `${endpoint}/node/explorer/accounts/${merchant.toAddress()}`);
 
     // 2. create vendor machine
-    const machine = {
+    let factoryAddress;
+    [hash, factoryAddress] = await client.createAssetFactory({
       moniker: `Vending_Machine_${merchant.toAddress()}`,
       readonly: false, // if we want to update the machine, we should set this to false
       transferrable: false,
-      data: {
-        type: 'AssetFactory',
-        value: {
-          description: 'Unlimited Vending Machine',
-          limit: 99999,
-          price: {
-            value: Buffer.from(fromTokenToUnit(1).toBuffer()),
-          },
-          template: JSON.stringify({ name: '{{name}}', brand: '{{brand}}' }),
-          allowedSpecArgs: ['name', 'brand'],
-          assetName: 'Product',
-          attributes: {
-            transferrable: false,
-          },
+      factory: {
+        description: 'Unlimited Vending Machine',
+        limit: 99999,
+        price: 1,
+        template: JSON.stringify({ name: '{{name}}', brand: '{{brand}}' }),
+        templateVariables: ['name', 'brand'],
+        assetName: 'Product',
+        attributes: {
+          transferrable: false,
         },
       },
-    };
-    const assetAddress = toAssetAddress(machine);
-    machine.address = assetAddress;
-    res = await client.sendCreateAssetTx({ tx: { itx: machine }, wallet: merchant });
-    console.log('factory template', machine.data.value.template);
-    console.log('view machine state', `${endpoint}/node/explorer/assets/${assetAddress}`);
-    console.log('create machine tx', `${endpoint}/node/explorer/txs/${res}`);
+      wallet: merchant,
+    });
+    console.log('view machine state', `${endpoint}/node/explorer/assets/${factoryAddress}`);
+    console.log('create machine tx', `${endpoint}/node/explorer/txs/${hash}`);
 
     // wait for machine state consolidates
     await sleep(3000);
 
     // 3. read machine
-    const { state } = await client.getAssetState({ address: assetAddress });
+    const { state } = await client.getAssetState({ address: factoryAddress });
     console.log('machine state', state);
 
     // 4. declare customer
-    res = await client.sendDeclareTx({ tx: { itx: { moniker: 'customer' } }, wallet: customer });
-    console.log('customer declare tx', res);
+    hash = await client.declare({ moniker: 'customer', wallet: customer });
+    console.log('customer declare tx', hash);
     console.log('customer view link', `${endpoint}/node/explorer/accounts/${customer.toAddress()}`);
 
     // 5. get some money
-    res = await client.sendPokeTx({
-      tx: {
-        nonce: 0,
-        itx: {
-          date: moment(new Date().toISOString())
-            .utc()
-            .format('YYYY-MM-DD'),
-          address: 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
-        },
-      },
-      wallet: customer,
-    });
-    console.log('poke tx', res);
-    console.log('poke link', `${endpoint}/node/explorer/txs/${res}`);
+    hash = await client.checkin({ wallet: customer });
+    console.log('poke tx', hash);
+    console.log('poke link', `${endpoint}/node/explorer/txs/${hash}`);
     await sleep(3000);
 
-    // 6. create product and its address
-    const product = {
-      ttl: 0,
-      readonly: true,
-      transferrable: false,
-      parent: machine.address,
-      data: {
-        type: 'Product',
-        value: { name: 'Coca', brand: 'Pepsi' },
-      },
-    };
-    const goodAddress = toAssetAddress(product);
-    console.log('product value', product.data.value);
-
-    // 7. acquire asset
-    res = await client.sendAcquireAssetTx({
-      tx: {
-        itx: {
-          to: machine.address,
-          specs: [
-            {
-              address: goodAddress,
-              data: JSON.stringify(product.data.value),
-            },
-          ],
-        },
-      },
+    // 6. acquire asset
+    let assetAddress;
+    [hash, [assetAddress]] = await client.acquireAsset({
+      assetFactory: factoryAddress,
+      assetName: 'Product',
+      assetVariables: [{ name: 'Coca', brand: 'Pepsi' }],
       wallet: customer,
     });
-    console.log('product spec', {
-      address: goodAddress,
-      data: JSON.stringify(product.data.value),
-    });
-    console.log('product address', goodAddress);
-    console.log('acquire tx', res);
-    console.log('acquire link', `${endpoint}/node/explorer/txs/${res}`);
+    console.log('product address', assetAddress);
+    console.log('acquire tx', hash);
+    console.log('acquire link', `${endpoint}/node/explorer/txs/${hash}`);
   } catch (err) {
     console.error(err);
     console.log(JSON.stringify(err.errors));
