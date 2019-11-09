f5324721388ff010fb2ff2857c6cf6ba4d39e62d
wangshijun <wangshijun2010@gmail.com>
2019-11-09 18:02:12 +0800

    chore: add memory storage for atomic-swap to easy test

--- did/did-auth/tests/handlers/wallet.spec.js
+++ did/did-auth/tests/handlers/wallet.spec.js
@@ -1,6 +1,37 @@
+const Mcrypto = require('@arcblock/mcrypto');
+const { fromRandom, WalletType } = require('@arcblock/forge-wallet');
+const Authenticator = require('../../lib').WalletAuthenticator;
 const Handlers = require('../../lib').WalletHandlers;
 
+const type = WalletType({
+  role: Mcrypto.types.RoleType.ROLE_APPLICATION,
+  pk: Mcrypto.types.KeyType.ED25519,
+  hash: Mcrypto.types.HashType.SHA3,
+});
+
+const wallet = fromRandom(type).toJSON();
+const chainHost = 'https://zinc.abtnetwork.io/api';
+const chainId = 'zinc-2019-05-17';
+
+const authenticator = new Authenticator({
+  wallet,
+  baseUrl: 'http://zinc.abtnetwork.io/webapp',
+  appInfo: {
+    name: 'ABT Wallet Demo',
+    description: 'Demo application to show the potential of ABT Wallet',
+    icon: 'https://arcblock.oss-cn-beijing.aliyuncs.com/images/wallet-round.png',
+  },
+  chainInfo: {
+    host: chainHost,
+    id: chainId,
+  },
+});
+
 describe('#Handlers', () => {
+  test('should be a function', () => {
+    expect(typeof Handlers).toEqual('function');
+  });
+
   test('should be a function', () => {
     expect(typeof Handlers).toEqual('function');
   });
---
+++ swap/swap-storage-memory/README.md
@@ -0,0 +1,37 @@
+# [**@arcblock/swap-storage-memory**](https://github.com/arcblock/forge-js)
+
+[![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+> Storage engine that uses memory to store data, implements interfaces defined in `@arcblock/swap-storage`.
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [Contributors](#contributors)
+
+## Install
+
+```sh
+npm install @arcblock/swap-storage-memory
+// or
+yarn add @arcblock/swap-storage-memory
+```
+
+## Usage
+
+```js
+const MemoryStorage = require('@arcblock/swap-storage-memory');
+
+const storage = new MemoryStorage();
+
+(async () => {
+  const item = await storage.create({});
+})();
+```
+
+## Contributors
+
+| Name           | Website                    |
+| -------------- | -------------------------- |
+| **wangshijun** | <https://ocap.arcblock.io> |
