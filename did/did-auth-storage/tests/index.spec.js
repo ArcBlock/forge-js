589b7a7e64337a083ed0bd4e434653e27dd55b8f
wangshijun <wangshijun2010@gmail.com>
2019-11-09 16:18:01 +0800

    chore: test coverage for did-auth-storage

--- did/did-auth-storage/tests/index.spec.js
+++ did/did-auth-storage/tests/index.spec.js
@@ -4,4 +4,60 @@ describe('#Storage', () => {
   test('should be a function', () => {
     expect(typeof Storage).toEqual('function');
   });
+
+  test('should extends event emitter', () => {
+    const storage = new Storage();
+    expect(typeof storage.on).toEqual('function');
+    expect(typeof storage.emit).toEqual('function');
+  });
+
+  test('should have create method', () => {
+    const storage = new Storage();
+    expect(typeof storage.create).toEqual('function');
+    try {
+      storage.create();
+    } catch (err) {
+      expect(err).toBeTruthy();
+    }
+  });
+
+  test('should have update method', () => {
+    const storage = new Storage();
+    expect(typeof storage.update).toEqual('function');
+    try {
+      storage.update();
+    } catch (err) {
+      expect(err).toBeTruthy();
+    }
+  });
+
+  test('should have delete method', () => {
+    const storage = new Storage();
+    expect(typeof storage.delete).toEqual('function');
+    try {
+      storage.delete();
+    } catch (err) {
+      expect(err).toBeTruthy();
+    }
+  });
+
+  test('should have read method', () => {
+    const storage = new Storage();
+    expect(typeof storage.read).toEqual('function');
+    try {
+      storage.read();
+    } catch (err) {
+      expect(err).toBeTruthy();
+    }
+  });
+
+  test('should have exist method', () => {
+    const storage = new Storage();
+    expect(typeof storage.exist).toEqual('function');
+    try {
+      storage.exist();
+    } catch (err) {
+      expect(err).toBeTruthy();
+    }
+  });
 });
