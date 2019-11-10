3617a6e858732eb44f70cbd68397e628430a8b5d
wangshijun <wangshijun2010@gmail.com>
2019-11-10 09:08:17 +0800

    chore: improve test case

--- did/did-auth-storage-memory/tests/index.spec.js
+++ did/did-auth-storage-memory/tests/index.spec.js
@@ -1,5 +1,26 @@
+const Storage = require('../lib');
+
 describe('#Storage', () => {
-  test('should have test', () => {
-    expect(true).toBeTruthy();
+  test('should be a function', () => {
+    expect(typeof Storage).toEqual('function');
+  });
+
+  test('should extends event emitter', () => {
+    const storage = new Storage();
+    expect(typeof storage.on).toEqual('function');
+    expect(typeof storage.emit).toEqual('function');
+  });
+
+  const methods = ['create', 'update', 'delete', 'read', 'exist'];
+  methods.forEach(x => {
+    test(`should have ${x} method`, () => {
+      const storage = new Storage();
+      expect(typeof storage[x]).toEqual('function');
+      try {
+        storage.create();
+      } catch (err) {
+        expect(err).toBeTruthy();
+      }
+    });
   });
 });
