9ab02c662da8dfc3136e4caa90be4410b71885f7
wangshijun <wangshijun2010@gmail.com>
2019-12-09 11:14:41 +0800

    chore: upgrade forge-proto with forge 1.0.2

--- forge/forge-proto/proto-full.js
+++ forge/forge-proto/proto-full.js
@@ -1,4 +1,4 @@
-// Auto generated code (2019-11-28T01:35:27.652Z), DO NOT EDIT
+// Auto generated code (2019-12-09T03:09:15.495Z), DO NOT EDIT
 const typePb = require('./lib/type_pb.js');
 const txPb = require('./lib/tx_pb.js');
 const traceTypePb = require('./lib/trace_type_pb.js');
@@ -6,29 +6,30 @@ const statePb = require('./lib/state_pb.js');
 const enumPb = require('./lib/enum_pb.js');
 const servicePb = require('./lib/service_pb.js');
 const rpcPb = require('./lib/rpc_pb.js');
-const revokeWithdrawTxPb = require('./lib/revoke_withdraw_tx_pb.js');
 const withdrawTokenTxPb = require('./lib/withdraw_token_tx_pb.js');
 const delegateTxPb = require('./lib/delegate_tx_pb.js');
 const accountMigrateTxPb = require('./lib/account_migrate_tx_pb.js');
 const transferTxPb = require('./lib/transfer_tx_pb.js');
-const revokeSwapTxPb = require('./lib/revoke_swap_tx_pb.js');
-const declareTxPb = require('./lib/declare_tx_pb.js');
 const revokeDelegateTxPb = require('./lib/revoke_delegate_tx_pb.js');
+const declareTxPb = require('./lib/declare_tx_pb.js');
+const revokeWithdrawTxPb = require('./lib/revoke_withdraw_tx_pb.js');
 const deactivateProtocolTxPb = require('./lib/deactivate_protocol_tx_pb.js');
-const refuelTxPb = require('./lib/refuel_tx_pb.js');
+const retrieveSwapTxPb = require('./lib/retrieve_swap_tx_pb.js');
 const createAssetTxPb = require('./lib/create_asset_tx_pb.js');
-const setupSwapTxPb = require('./lib/setup_swap_tx_pb.js');
+const pokeTxPb = require('./lib/poke_tx_pb.js');
 const consumeAssetTxPb = require('./lib/consume_asset_tx_pb.js');
-const depositTokenTxPb = require('./lib/deposit_token_tx_pb.js');
+const exchangeTxPb = require('./lib/exchange_tx_pb.js');
+const updateAssetTxPb = require('./lib/update_asset_tx_pb.js');
+const deployProtocolTxPb = require('./lib/deploy_protocol_tx_pb.js');
 const acquireAssetTxPb = require('./lib/acquire_asset_tx_pb.js');
 const activateProtocolTxPb = require('./lib/activate_protocol_tx_pb.js');
 const approveWithdrawTxPb = require('./lib/approve_withdraw_tx_pb.js');
 const upgradeNodeTxPb = require('./lib/upgrade_node_tx_pb.js');
-const updateAssetTxPb = require('./lib/update_asset_tx_pb.js');
-const retrieveSwapTxPb = require('./lib/retrieve_swap_tx_pb.js');
-const deployProtocolTxPb = require('./lib/deploy_protocol_tx_pb.js');
-const exchangeTxPb = require('./lib/exchange_tx_pb.js');
-const pokeTxPb = require('./lib/poke_tx_pb.js');
+const updateValidatorTxPb = require('./lib/update_validator_tx_pb.js');
+const refuelTxPb = require('./lib/refuel_tx_pb.js');
+const revokeSwapTxPb = require('./lib/revoke_swap_tx_pb.js');
+const depositTokenTxPb = require('./lib/deposit_token_tx_pb.js');
+const setupSwapTxPb = require('./lib/setup_swap_tx_pb.js');
 const serviceGrpcPb = require('./lib/service_grpc_pb.js');
 const vendorPb = require('./lib/vendor_pb.js');
 
@@ -41,29 +42,30 @@ const forgeTypes = Object.assign(
   enumPb,
   servicePb,
   rpcPb,
-  revokeWithdrawTxPb,
   withdrawTokenTxPb,
   delegateTxPb,
   accountMigrateTxPb,
   transferTxPb,
-  revokeSwapTxPb,
-  declareTxPb,
   revokeDelegateTxPb,
+  declareTxPb,
+  revokeWithdrawTxPb,
   deactivateProtocolTxPb,
-  refuelTxPb,
+  retrieveSwapTxPb,
   createAssetTxPb,
-  setupSwapTxPb,
+  pokeTxPb,
   consumeAssetTxPb,
-  depositTokenTxPb,
+  exchangeTxPb,
+  updateAssetTxPb,
+  deployProtocolTxPb,
   acquireAssetTxPb,
   activateProtocolTxPb,
   approveWithdrawTxPb,
   upgradeNodeTxPb,
-  updateAssetTxPb,
-  retrieveSwapTxPb,
-  deployProtocolTxPb,
-  exchangeTxPb,
-  pokeTxPb
+  updateValidatorTxPb,
+  refuelTxPb,
+  revokeSwapTxPb,
+  depositTokenTxPb,
+  setupSwapTxPb
 );
 const forgeServices = Object.assign({}, serviceGrpcPb);
 const vendorTypes = Object.assign({}, vendorPb);
--- forge/forge-proto/proto-lite.js
+++ forge/forge-proto/proto-lite.js
@@ -1,29 +1,30 @@
-// Auto generated code (2019-11-28T01:35:27.852Z), DO NOT EDIT
+// Auto generated code (2019-12-09T03:09:15.832Z), DO NOT EDIT
 const typePb = require('./lib/type_pb.js');
 const enumPb = require('./lib/enum_pb.js');
 const revokeDelegateTxPb = require('./lib/revoke_delegate_tx_pb.js');
 const withdrawTokenTxPb = require('./lib/withdraw_token_tx_pb.js');
-const declareTxPb = require('./lib/declare_tx_pb.js');
-const accountMigrateTxPb = require('./lib/account_migrate_tx_pb.js');
 const setupSwapTxPb = require('./lib/setup_swap_tx_pb.js');
-const depositTokenTxPb = require('./lib/deposit_token_tx_pb.js');
+const accountMigrateTxPb = require('./lib/account_migrate_tx_pb.js');
 const deactivateProtocolTxPb = require('./lib/deactivate_protocol_tx_pb.js');
-const pokeTxPb = require('./lib/poke_tx_pb.js');
+const depositTokenTxPb = require('./lib/deposit_token_tx_pb.js');
 const createAssetTxPb = require('./lib/create_asset_tx_pb.js');
-const retrieveSwapTxPb = require('./lib/retrieve_swap_tx_pb.js');
+const pokeTxPb = require('./lib/poke_tx_pb.js');
 const transferTxPb = require('./lib/transfer_tx_pb.js');
-const revokeSwapTxPb = require('./lib/revoke_swap_tx_pb.js');
+const retrieveSwapTxPb = require('./lib/retrieve_swap_tx_pb.js');
 const consumeAssetTxPb = require('./lib/consume_asset_tx_pb.js');
+const revokeSwapTxPb = require('./lib/revoke_swap_tx_pb.js');
+const approveWithdrawTxPb = require('./lib/approve_withdraw_tx_pb.js');
 const deployProtocolTxPb = require('./lib/deploy_protocol_tx_pb.js');
-const exchangeTxPb = require('./lib/exchange_tx_pb.js');
+const declareTxPb = require('./lib/declare_tx_pb.js');
 const activateProtocolTxPb = require('./lib/activate_protocol_tx_pb.js');
 const upgradeNodeTxPb = require('./lib/upgrade_node_tx_pb.js');
+const updateValidatorTxPb = require('./lib/update_validator_tx_pb.js');
 const updateAssetTxPb = require('./lib/update_asset_tx_pb.js');
-const approveWithdrawTxPb = require('./lib/approve_withdraw_tx_pb.js');
-const refuelTxPb = require('./lib/refuel_tx_pb.js');
+const exchangeTxPb = require('./lib/exchange_tx_pb.js');
 const delegateTxPb = require('./lib/delegate_tx_pb.js');
 const revokeWithdrawTxPb = require('./lib/revoke_withdraw_tx_pb.js');
 const acquireAssetTxPb = require('./lib/acquire_asset_tx_pb.js');
+const refuelTxPb = require('./lib/refuel_tx_pb.js');
 
 const vendorPb = require('./lib/vendor_pb.js');
 
@@ -33,27 +34,28 @@ const forgeTypes = Object.assign(
   enumPb,
   revokeDelegateTxPb,
   withdrawTokenTxPb,
-  declareTxPb,
-  accountMigrateTxPb,
   setupSwapTxPb,
-  depositTokenTxPb,
+  accountMigrateTxPb,
   deactivateProtocolTxPb,
-  pokeTxPb,
+  depositTokenTxPb,
   createAssetTxPb,
-  retrieveSwapTxPb,
+  pokeTxPb,
   transferTxPb,
-  revokeSwapTxPb,
+  retrieveSwapTxPb,
   consumeAssetTxPb,
+  revokeSwapTxPb,
+  approveWithdrawTxPb,
   deployProtocolTxPb,
-  exchangeTxPb,
+  declareTxPb,
   activateProtocolTxPb,
   upgradeNodeTxPb,
+  updateValidatorTxPb,
   updateAssetTxPb,
-  approveWithdrawTxPb,
-  refuelTxPb,
+  exchangeTxPb,
   delegateTxPb,
   revokeWithdrawTxPb,
-  acquireAssetTxPb
+  acquireAssetTxPb,
+  refuelTxPb
 );
 const forgeServices = {};
 const vendorTypes = Object.assign({}, vendorPb);
