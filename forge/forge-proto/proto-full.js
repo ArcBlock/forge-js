1e459fbb5e4ecacd1c8b769003de32b9c1bc0d2b
wangshijun <wangshijun2010@gmail.com>
2020-03-25 16:20:15 +0800

    fix: lite and full proto

--- forge/forge-proto/proto-full.js
+++ forge/forge-proto/proto-full.js
@@ -1,8 +1,8 @@
-// Auto generated code (2020-03-25T07:50:37.733Z), DO NOT EDIT
+// Auto generated code (2020-03-25T08:18:29.697Z), DO NOT EDIT
+const enumPb = require('./lib/enum_pb.js');
 const rpcPb = require('./lib/rpc_pb.js');
 const servicePb = require('./lib/service_pb.js');
 const statePb = require('./lib/state_pb.js');
-const enumPb = require('./lib/enum_pb.js');
 const traceTypePb = require('./lib/trace_type_pb.js');
 const txPb = require('./lib/tx_pb.js');
 const typePb = require('./lib/type_pb.js');
@@ -10,7 +10,7 @@ const serviceGrpcPb = require('./lib/service_grpc_pb.js');
 const vendorPb = require('./lib/vendor_pb.js');
 
 
-const forgeTypes = Object.assign({}, rpcPb, servicePb, statePb, enumPb, traceTypePb, txPb, typePb);
+const forgeTypes = Object.assign({}, enumPb, rpcPb, servicePb, statePb, traceTypePb, txPb, typePb);
 const forgeServices = Object.assign({}, serviceGrpcPb);
 const vendorTypes = Object.assign({}, vendorPb);
 const vendorServices = {};
