83f2227cfbf7bcfbca19cb99cf2560fa581a4d3a
wangshijun <wangshijun2010@gmail.com>
2020-04-13 15:17:25 +0800

    chore: make jwt sign compatible with forge-web (#452)
    * chore: make jwt sign compatible with forge-web
    
    * chore: bump version

--- forge/graphql-client/examples/declare_forgeweb.js
+++ forge/graphql-client/examples/declare_forgeweb.js
@@ -6,16 +6,15 @@
  *
  * In real world, identities may belong to different entities: application, user, node, device
  *
- * Run script with: `DEBUG=@arcblock/graphql-client node examples/declare.js`
+ * Run script with: `DEBUG=@arcblock/graphql-client node examples/declare_forgeweb.js`
  */
 const axios = require('axios');
-// const GraphqlClient = require('@arcblock/graphql-client');
 const { JWT } = require('@arcblock/did-auth');
 const { fromRandom } = require('@arcblock/forge-wallet');
 const { toBase58 } = require('@arcblock/forge-util');
 
-const endpoint = process.env.FORGE_API_HOST || 'http://127.0.0.1:8210'; // testnet
-// const client = new GraphqlClient(`${endpoint}/api`);
+// const endpoint = process.env.FORGE_API_HOST || 'http://127.0.0.1:8210';
+const endpoint = process.env.FORGE_API_HOST || 'https://xenon.abtnetwork.io';
 
 axios.defaults.baseURL = endpoint;
 
@@ -39,7 +38,11 @@ axios.defaults.baseURL = endpoint;
     console.log('Step2', info);
 
     const { requestedClaims } = info;
-    requestedClaims[0].sig = toBase58(user.sign(requestedClaims[0].digest));
+    const { origin, digest } = requestedClaims[0];
+    if (toBase58(user.hash(origin)) !== digest) {
+      throw new Error('Origin and digest do not match');
+    }
+    requestedClaims[0].sig = toBase58(user.sign(origin));
 
     token = JWT.sign(user.toAddress(), user.secretKey, { requestedClaims });
     payload = {
