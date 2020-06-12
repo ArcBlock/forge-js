0063fcd59df28702dfda3986ac6586bb73b1a74a
wangshijun <wangshijun2010@gmail.com>
2020-06-13 06:27:08 +0800

    chore: regenerate forge-abi code with latest grpc tools

--- forge/forge-proto/proto-lite.js
+++ forge/forge-proto/proto-lite.js
@@ -1,7 +1,6 @@
-// Auto generated code (2020-03-25T23:21:59.921Z), DO NOT EDIT
+// Auto generated code (2020-06-12T22:20:55.270Z), DO NOT EDIT
 const enumPb = require('./lib/enum_pb.js');
 const rpcPb = require('./lib/rpc_pb.js');
-const servicePb = require('./lib/service_pb.js');
 const statePb = require('./lib/state_pb.js');
 const traceTypePb = require('./lib/trace_type_pb.js');
 const txPb = require('./lib/tx_pb.js');
@@ -9,8 +8,7 @@ const typePb = require('./lib/type_pb.js');
 
 const vendorPb = require('./lib/vendor_pb.js');
 
-
-const forgeTypes = Object.assign({}, enumPb, rpcPb, servicePb, statePb, traceTypePb, txPb, typePb);
+const forgeTypes = Object.assign({}, enumPb, rpcPb, statePb, traceTypePb, txPb, typePb);
 const forgeServices = {};
 const vendorTypes = Object.assign({}, vendorPb);
 const vendorServices = {};
